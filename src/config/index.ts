25b6a5b1fed00eb22e8ccb114336e55b01ff84c1
DiegoxK <diego.stv@outlook.es>
2024-05-29 14:03:37 -0500

    Added configuration

---
+++ src/config/index.ts
@@ -0,0 +1,16 @@
+export const siteConfig = {
+  navigation: [
+    {
+      label: "Inicio",
+      url: "/",
+    },
+    {
+      label: "Academia",
+      url: "/academy",
+    },
+    {
+      label: "Nosotros",
+      url: "/about",
+    },
+  ],
+};
