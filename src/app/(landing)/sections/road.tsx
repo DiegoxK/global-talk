26b162427b714160fe9bad301cd81c8b5f15f4de
DiegoxK <diego.stv@outlook.es>
2024-05-30 09:09:01 -0500

    Finished basic road section layout

---
+++ src/app/(landing)/sections/road.tsx
@@ -0,0 +1,55 @@
+export default function Road() {
+  return (
+    <section className="py-16">
+      <h2>¿Por qué estudiar con nosotros?</h2>
+      <div className="grid grid-cols-4">
+        <div>
+          <h3>A2</h3>
+          <ul>
+            <li>Comunicación básica en situaciones cotidianas.</li>
+            <li>Puedes entender y usar frases comunes. </li>
+          </ul>
+        </div>
+        <div>
+          <h3>B1</h3>
+          <ul>
+            <li>Conversación fluida en temas comunes.</li>
+            <li>
+              Capacidad de entender puntos principales de textos sencillos.
+            </li>
+            <li>Calificación para roles administrativos básicos.</li>
+          </ul>
+        </div>
+        <div>
+          <h3>B2</h3>
+          <ul>
+            <li>Comprensión y producción de textos complejos.</li>
+            <li>
+              Habilidad para interactuar con fluidez con hablantes nativos
+            </li>
+            <li>Estudios universitarios en inglés.</li>
+            <li>Elegibilidad para trabajos profesionales.</li>
+          </ul>
+        </div>
+        <div>
+          <h3>C1</h3>
+          <ul>
+            <li>
+              Comunicación efectiva en contextos profesionales y académicos.
+            </li>
+            <li>
+              Comunicación efectiva en contextos profesionales y académicos.
+            </li>
+            <li>
+              Mayores oportunidades laborales en posiciones de alta
+              responsabilidad.
+            </li>
+            <li>
+              Acceso a programas de posgrado en universidades de habla inglesa.
+            </li>
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
+}
