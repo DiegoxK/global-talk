8ba74d0e438af6668c1c0a2a9f67f2768d6f7b23
DiegoxK <diego.stv@outlook.es>
2024-05-30 07:06:47 -0500

    Moved main page

---
+++ src/app/(landing)/page.tsx
@@ -0,0 +1,11 @@
+import Hero from "./sections/hero";
+import Qualities from "./sections/qualities";
+
+export default async function Home() {
+  return (
+    <>
+      <Hero />
+      <Qualities />
+    </>
+  );
+}
