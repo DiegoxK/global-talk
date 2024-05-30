58f57c4ad4491660192652211d5d9ac15044d3f5
DiegoxK <diego.stv@outlook.es>
2024-05-30 09:09:34 -0500

    Fixed a react atribute

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,11 +1,15 @@
 import Hero from "./sections/hero";
 import Qualities from "./sections/qualities";
+import Road from "./sections/road";
+import Team from "./sections/team";
 
 export default async function Home() {
   return (
     <>
       <Hero />
       <Qualities />
+      <Road />
+      <Team />
     </>
   );
 }
