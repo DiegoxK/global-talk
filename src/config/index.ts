b304252ffd8db83df526877e293f1f01eac56459
DiegoxK <diego.stv@outlook.es>
2024-05-30 12:35:28 -0500

    Added sublinks to navigation menu

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,14 +3,40 @@ export const siteConfig = {
     {
       label: "Inicio",
       url: "/",
+      subLinks: [
+        { label: "Caracteristicas", url: "/#qualities" },
+        { label: "Profesores", url: "/#team" },
+        { label: "Preguntas", url: "/#questions" },
+      ],
     },
     {
       label: "Academia",
       url: "/academy",
+      subLinks: [
+        {
+          label: "Herramientas educativas",
+          url: "/academy#tools",
+        },
+        { label: "Camino a la certificación", url: "/academy#certification" },
+      ],
     },
     {
       label: "Nosotros",
       url: "/about",
+      subLinks: [
+        { label: "Misión", url: "/about#mision" },
+        { label: "Visión", url: "/about#vision" },
+        { label: "Experiencia", url: "/about#experience" },
+        { label: "Profesores", url: "/about#teams" },
+      ],
+    },
+    {
+      label: "Contacto",
+      url: "/contact",
+      subLinks: [
+        { label: "Información", url: "/contact#info" },
+        { label: "Formulario", url: "/contact#form" },
+      ],
     },
   ],
 };
