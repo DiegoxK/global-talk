8cd8a448cbbf7003e7868766e17c880d430efdb5
DiegoxK <diego.stv@outlook.es>
2024-05-30 07:10:04 -0500

    Adjust header styling for better alignment

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,7 +4,7 @@ import MobileNav from "./navigation/mobile-nav";
 export default function Header() {
   return (
     <header className="w-full">
-      <div className="mx-4 flex items-center py-4">
+      <div className="mx-10 flex items-center py-6">
         <div className="flex w-full items-center justify-between">
           <MainNav />
           <MobileNav />
