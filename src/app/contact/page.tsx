ae0655d338acdc5d3bc80f285590e963ca29b3ce
DiegoxK <diego.stv@outlook.es>
2024-05-30 21:08:23 -0500

    Update image source in Contact page for better file path handling

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -6,7 +6,7 @@ export default function Contact() {
   return (
     <>
       <div className="flex w-full flex-col xl:flex-row">
-        <Image unoptimized src="/contact/group.svg" alt="Imagen decorativa" />
+        <img src="/contact/group.svg" alt="Imagen decorativa" />
         <div className="w-full">
           <div className="flex items-center justify-center bg-primary-800 px-3 py-16 xl:h-[606px]">
             <div className="w-fit rounded-sm bg-primary-200 px-6 md:px-8">
