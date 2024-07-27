23979dabc0892119f95a26da299645c7db4b6cd6
DiegoxK <diego.stv@outlook.es>
2024-07-27 11:22:51 -0500

    Add server-side authentication check in RootLayout

--- src/app/(routes)/academy/layout.tsx
+++ src/app/(routes)/academy/layout.tsx
@@ -1,8 +1,17 @@
-export default function RootLayout({
+import { getServerAuthSession } from "@/server/auth";
+import { redirect } from "next/navigation";
+
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const session = await getServerAuthSession();
+
+  if (!session) {
+    redirect("/auth/login");
+  }
+
   return (
     <>
       <main>{children}</main>
