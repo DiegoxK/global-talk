12e4f8b94a724c7cea65da903d46b7b6bf32b8b1
DiegoxK <diego.stv@outlook.es>
2024-07-27 11:21:21 -0500

    Finished basic email token verification

---
+++ src/app/(routes)/auth/verify-request/_components/otp-form.tsx
@@ -0,0 +1,108 @@
+"use client";
+
+import { REGEXP_ONLY_DIGITS_AND_CHARS } from "input-otp";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+
+import {
+  InputOTP,
+  InputOTPGroup,
+  InputOTPSlot,
+} from "@/components/ui/input-otp";
+import { useRouter } from "next/navigation";
+import { getBaseUrl } from "@/trpc/react";
+
+import { useState } from "react";
+
+const FormSchema = z.object({
+  pin: z.string().toLowerCase().min(6, {
+    message: "Tu clave de un solo uso debe tener 6 caracteres.",
+  }),
+});
+
+export function InputOTPForm({ email }: { email: string }) {
+  const [loading, setLoading] = useState(false);
+
+  const router = useRouter();
+
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      pin: "",
+    },
+  });
+
+  function onSubmit(data: z.infer<typeof FormSchema>) {
+    setLoading(true);
+
+    const callbackUrl = encodeURIComponent(`${getBaseUrl()}/academy`);
+    const token = encodeURIComponent(data.pin);
+    const emailEncoded = encodeURIComponent(email);
+
+    router.replace(
+      `/api/auth/callback/email?callbackUrl=${callbackUrl}&token=${token}&email=${emailEncoded}`,
+    );
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-2">
+        <FormField
+          control={form.control}
+          name="pin"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="text-white">
+                Clave de un solo uso.
+              </FormLabel>
+              <FormControl className="justify-center">
+                <InputOTP
+                  disabled={loading}
+                  containerClassName="justify-center"
+                  maxLength={6}
+                  {...field}
+                  pattern={REGEXP_ONLY_DIGITS_AND_CHARS}
+                >
+                  <InputOTPGroup className="rounded-md border-2 border-primary bg-white uppercase">
+                    <InputOTPSlot index={0} />
+                    <InputOTPSlot index={1} />
+                    <InputOTPSlot index={2} />
+                    <InputOTPSlot index={3} />
+                    <InputOTPSlot index={4} />
+                    <InputOTPSlot index={5} />
+                  </InputOTPGroup>
+                </InputOTP>
+              </FormControl>
+
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button
+          disabled={loading}
+          className="w-full rounded-md bg-primary-800"
+          type="submit"
+        >
+          {loading ? <div className="loader"></div> : <span>Verificar</span>}
+        </Button>
+        <FormDescription className="text-white">
+          El código fue enviado a tu correo electrónico{" "}
+          <span className="font-bold text-primary-900">{email}</span>.
+        </FormDescription>
+      </form>
+    </Form>
+  );
+}
--- src/app/(routes)/auth/verify-request/page.tsx
+++ src/app/(routes)/auth/verify-request/page.tsx
@@ -1,3 +1,33 @@
-export default function VerifyRequest() {
-  return <div>VerifyRequest</div>;
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
+import { InputOTPForm } from "./_components/otp-form";
+
+import { Fingerprint } from "lucide-react";
+import { getServerAuthSession } from "@/server/auth";
+
+export default async function VerificationPage() {
+  const session = await getServerAuthSession();
+
+  if (session) {
+    redirect("/academy");
+  }
+
+  const email = cookies().get("otp-email")?.value;
+
+  if (!email) {
+    redirect("/auth/login");
+  }
+
+  return (
+    <main className="bg-pattern flex min-h-screen flex-col items-center justify-center">
+      <div className="flex max-w-[400px] flex-col items-center text-center">
+        <Fingerprint size={80} className="text-white" />
+        <h1 className="text-4xl font-bold text-transparent text-white">
+          Revisa tu Correo
+        </h1>
+        <InputOTPForm email={email} />
+      </div>
+    </main>
+  );
 }
