3155c9f9726935a06efe6c8cc314ebe97b23b138
DiegoxK <diego.stv@outlook.es>
2024-07-29 17:39:24 -0500

    Added TeacherNav component for academy teacher navigation

---
+++ src/app/(routes)/academy/_components/navigation/teacher-nav.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { Separator } from "@/components/ui/separator";
+import { NavLink } from "./nav-link";
+import { BookOpenCheck } from "lucide-react";
+
+export default function TeacherNav() {
+  return (
+    <>
+      <Separator className="my-2" />
+      <NavLink Icon={BookOpenCheck} location="/academy" href="/profesor">
+        Profesor
+      </NavLink>
+    </>
+  );
+}
