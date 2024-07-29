97e0dff079367a429a64019f6261df8ac302f986
DiegoxK <diego.stv@outlook.es>
2024-07-29 17:22:25 -0500

    Update UserNav component with profile and logout buttons

--- src/app/(routes)/academy/_components/navigation/user-nav.tsx
+++ src/app/(routes)/academy/_components/navigation/user-nav.tsx
@@ -1,3 +1,23 @@
+import { Button } from "@/components/ui/button";
+import { DoorClosed, UserRoundPen } from "lucide-react";
+
 export default function UserNav() {
-  return <div>UserNav</div>;
+  return (
+    <div className="flex flex-col gap-2">
+      <Button
+        className="justify-start border-primary text-primary"
+        variant="outline"
+      >
+        <UserRoundPen className="mr-2 h-5 w-5" />
+        Mi perfil
+      </Button>
+      <Button
+        className="justify-start border-destructive text-destructive hover:bg-red-50 hover:text-destructive"
+        variant="outline"
+      >
+        <DoorClosed className="mr-2 h-5 w-5" />
+        Cerrar sesión
+      </Button>
+    </div>
+  );
 }
