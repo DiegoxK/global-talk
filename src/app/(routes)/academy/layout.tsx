34363ea2243c6e4892f002c45c09d00bd37a6923
DiegoxK <diego.stv@outlook.es>
2024-07-29 17:39:10 -0500

    Update academy layout

--- src/app/(routes)/academy/layout.tsx
+++ src/app/(routes)/academy/layout.tsx
@@ -17,13 +17,13 @@ export default async function RootLayout({
   return (
     <div className="flex">
       <Sidebar user={session.user} />
-      <main className="m-4 w-full">
+      <div className="m-4 w-full">
         <div className="flex w-full items-center justify-center gap-1 rounded-lg bg-primary-700 py-10 text-3xl font-extrabold text-white">
           <Logo className="fill-white" height={40} width={38} />
           lobal talk medallo
         </div>
-        {children}
-      </main>
+        <main className="overflow-auto">{children}</main>
+      </div>
     </div>
   );
 }
