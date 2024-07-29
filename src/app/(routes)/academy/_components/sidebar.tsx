9483486b08f32dac4081abc5548910174bd62f43
DiegoxK <diego.stv@outlook.es>
2024-07-29 17:12:37 -0500

    Created sidebar component

---
+++ src/app/(routes)/academy/_components/sidebar.tsx
@@ -0,0 +1,44 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { env } from "@/env";
+import type { User } from "@/lib/definitions";
+import { Logo } from "@/vectors/logo";
+import { UserRound } from "lucide-react";
+import Navigation from "./navigation/navigation";
+import UserNav from "./navigation/user-nav";
+
+interface SidebarProps {
+  user: User;
+}
+
+export default function Sidebar({ user }: SidebarProps) {
+  return (
+    <div className="flex h-screen w-96 flex-col space-y-2 bg-primary-50 p-2">
+      <div className="flex flex-col items-center justify-center text-nowrap rounded-md bg-white py-8">
+        <Avatar className="h-24 w-24 bg-primary-700">
+          <AvatarImage src={user.image ?? undefined} />
+          <AvatarFallback className="bg-transparent">
+            <Logo className="fill-white" height={40} width={38} />
+          </AvatarFallback>
+        </Avatar>
+        <p className="mt-1 text-lg font-bold">
+          {user.name + " " + user.last_name}
+        </p>
+        <p className="mt-[-5px] font-thin">{user.email}</p>
+        <p className="mt-1 font-semibold text-primary-700">
+          <UserRound className="mr-2 inline-block" />
+          {user.userRole === env.STUDENT_ROLE
+            ? "Estudiante"
+            : user.userRole === env.TEACHER_ROLE
+              ? "Profesor"
+              : "Administrador"}
+        </p>
+        {/* TODO: add user level to schema */}
+        <p className="font-light text-primary-400">A0: Principiante</p>
+      </div>
+      <div className="flex grow flex-col justify-between">
+        <Navigation location={"/academy"} />
+        <UserNav />
+      </div>
+    </div>
+  );
+}
