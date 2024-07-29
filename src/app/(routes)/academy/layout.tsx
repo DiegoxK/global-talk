e27c722980079c93747b02a02634858245713980
DiegoxK <diego.stv@outlook.es>
2024-07-29 17:12:53 -0500

    AUpdate academy layout with sidebar and logo

--- src/app/(routes)/academy/layout.tsx
+++ src/app/(routes)/academy/layout.tsx
@@ -1,5 +1,7 @@
 import { getServerAuthSession } from "@/server/auth";
 import { redirect } from "next/navigation";
+import Sidebar from "./_components/sidebar";
+import { Logo } from "@/vectors/logo";
 
 export default async function RootLayout({
   children,
@@ -13,8 +15,15 @@ export default async function RootLayout({
   }
 
   return (
-    <>
-      <main>{children}</main>
-    </>
+    <div className="flex">
+      <Sidebar user={session.user} />
+      <main className="m-4 w-full">
+        <div className="flex w-full items-center justify-center gap-1 rounded-lg bg-primary-700 py-10 text-3xl font-extrabold text-white">
+          <Logo className="fill-white" height={40} width={38} />
+          lobal talk medallo
+        </div>
+        {children}
+      </main>
+    </div>
   );
 }
