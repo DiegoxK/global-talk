3b12f291fea1286597d1bce9fdfc4d5f557a35a9
DiegoxK <diego.stv@outlook.es>
2024-07-30 18:47:41 -0500

    Add lectures page with dynamic rendering of lecture cards

---
+++ src/app/(routes)/academy/lectures/[levelId]/page.tsx
@@ -0,0 +1,87 @@
+import { api } from "@/trpc/server";
+import LectureCard from "../../_components/ui/lecture-card";
+
+interface LecturesProps {
+  params: { levelId: string };
+}
+
+export default async function Lectures({ params: { levelId } }: LecturesProps) {
+  const lectures = await api.lecture.getLectures({
+    levelId,
+  });
+
+  return (
+    <div className="grid h-fit w-full grid-cols-1 gap-3 pb-4 pt-4 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
+      {lectures.map(async (lecture) => {
+        const scheduleNumber = await api.schedule.scheduleNumber({
+          lectureId: lecture.id,
+        });
+
+        return (
+          <>
+            <LectureCard
+              key={lecture.id}
+              lecture={lecture}
+              scheduleCount={scheduleNumber?.count}
+            />
+            <LectureCard
+              key={lecture.id}
+              lecture={lecture}
+              scheduleCount={scheduleNumber?.count}
+            />
+            <LectureCard
+              key={lecture.id}
+              lecture={lecture}
+              scheduleCount={scheduleNumber?.count}
+            />
+            <LectureCard
+              key={lecture.id}
+              lecture={lecture}
+              scheduleCount={scheduleNumber?.count}
+            />
+            <LectureCard
+              key={lecture.id}
+              lecture={lecture}
+              scheduleCount={scheduleNumber?.count}
+            />
+            <LectureCard
+              key={lecture.id}
+              lecture={lecture}
+              scheduleCount={scheduleNumber?.count}
+            />
+            <LectureCard
+              key={lecture.id}
+              lecture={lecture}
+              scheduleCount={scheduleNumber?.count}
+            />
+            <LectureCard
+              key={lecture.id}
+              lecture={lecture}
+              scheduleCount={scheduleNumber?.count}
+            />
+            <LectureCard
+              key={lecture.id}
+              lecture={lecture}
+              scheduleCount={scheduleNumber?.count}
+            />
+            <LectureCard
+              key={lecture.id}
+              lecture={lecture}
+              scheduleCount={scheduleNumber?.count}
+            />
+            <LectureCard
+              key={lecture.id}
+              lecture={lecture}
+              scheduleCount={scheduleNumber?.count}
+            />
+            <LectureCard
+              key={lecture.id}
+              lecture={lecture}
+              scheduleCount={scheduleNumber?.count}
+            />
+          </>
+        );
+      })}
+    </div>
+  );
+}
--- src/app/(routes)/academy/lectures/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Lectures() {
-  return <div>Clases</div>;
-}
