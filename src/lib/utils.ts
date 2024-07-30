c2116cf1846473ff84087fd33d60c5a20c10157d
DiegoxK <diego.stv@outlook.es>
2024-07-30 10:56:18 -0500

    Added formatDate and formatTime utility functions

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,45 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function formatDate(input: string): string {
+  const date = new Date(input);
+
+  const day = date.getDate();
+  const month = date.getMonth() + 1; // Months are zero-indexed
+  const year = date.getFullYear();
+
+  const monthNames = [
+    "Enero",
+    "Febrero",
+    "Marzo",
+    "Abril",
+    "Mayo",
+    "Junio",
+    "Julio",
+    "Agosto",
+    "Septiembre",
+    "Octubre",
+    "Noviembre",
+    "Diciembre",
+  ];
+  const monthName = monthNames[month];
+
+  return `${day} de ${monthName} de ${year}`;
+}
+
+export function formatTime(timeString: string): string {
+  const formattedTime = new Date(
+    "1970-01-01T" + timeString + "Z",
+  ).toLocaleTimeString("en-US", {
+    timeZone: "UTC",
+    hour12: true,
+    hour: "numeric",
+    minute: "numeric",
+  });
+
+  return formattedTime;
 }
