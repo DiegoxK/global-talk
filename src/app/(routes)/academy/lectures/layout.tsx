bbe8511729b8fcd8bcdda1fa6d0b0a08e152ddf2
DiegoxK <diego.stv@outlook.es>
2024-07-30 18:47:50 -0500

    Add LecturesLayout component for academy lectures page

---
+++ src/app/(routes)/academy/lectures/layout.tsx
@@ -0,0 +1,17 @@
+import { api } from "@/trpc/server";
+import RightSidebar from "../_components/ui/right-sidebar";
+
+export default async function LecturesLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const levels = await api.level.getLevels();
+
+  return (
+    <div className="flex justify-between">
+      {children}
+      <RightSidebar levels={levels} />
+    </div>
+  );
+}
