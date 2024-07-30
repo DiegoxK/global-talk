4948d33a104455b665222b21545a7a4e1730e7ba
DiegoxK <diego.stv@outlook.es>
2024-07-30 18:48:26 -0500

    Update RootLayout component in academy route

--- src/app/(routes)/academy/layout.tsx
+++ src/app/(routes)/academy/layout.tsx
@@ -2,6 +2,7 @@ import { getServerAuthSession } from "@/server/auth";
 import { redirect } from "next/navigation";
 import Sidebar from "./_components/ui/sidebar";
 import { Logo } from "@/vectors/logo";
+import { api } from "@/trpc/server";
 
 export default async function RootLayout({
   children,
@@ -9,6 +10,13 @@ export default async function RootLayout({
   children: React.ReactNode;
 }) {
   const session = await getServerAuthSession();
+  // const course = await api.course.getCourseName();
+  // const firstLevel = await api.level.getFirstLevel();
+
+  const [course, firstLevel] = await Promise.all([
+    api.course.getCourseName(),
+    api.level.getFirstLevel(),
+  ]);
 
   if (!session) {
     redirect("/auth/login");
@@ -16,7 +24,7 @@ export default async function RootLayout({
 
   return (
     <div className="flex">
-      <Sidebar user={session.user} />
+      <Sidebar user={session.user} course={course} firstLevel={firstLevel} />
       <div className="mx-4 mt-4 flex w-full flex-col">
         {/* TODO: Imagen? */}
         <div className="flex w-full items-center justify-center gap-1 rounded-lg bg-primary-700 py-10 text-3xl font-extrabold text-white">
