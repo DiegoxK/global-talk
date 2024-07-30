20fe23c11ad8829118a2a3a36c58b63c8cfd46ad
DiegoxK <diego.stv@outlook.es>
2024-07-30 10:57:17 -0500

    Added lecture cards to the home page

--- src/app/(routes)/academy/(home)/page.tsx
+++ src/app/(routes)/academy/(home)/page.tsx
@@ -1,3 +1,24 @@
-export default function Home() {
-  return <div>Dashboard</div>;
+import { api } from "@/trpc/server";
+import LectureCard from "../_components/ui/lecture-card";
+
+export default async function Home() {
+  const lectures = await api.lecture.getLectures();
+
+  return (
+    <div className="flex gap-4">
+      {lectures.map(async (lecture) => {
+        const scheduleNumber = await api.schedule.scheduleNumber({
+          lectureId: lecture.id,
+        });
+
+        return (
+          <LectureCard
+            key={lecture.id}
+            lecture={lecture}
+            scheduleCount={scheduleNumber?.count}
+          />
+        );
+      })}
+    </div>
+  );
 }
