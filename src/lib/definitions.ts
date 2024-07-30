c8288295d8b39c221831f63b721cefd1bdf15bb2
DiegoxK <diego.stv@outlook.es>
2024-07-30 10:56:35 -0500

    Added Lecture type definition

--- src/lib/definitions.ts
+++ src/lib/definitions.ts
@@ -1,4 +1,9 @@
 import type { z } from "zod";
 import type { userSchema } from "@/server/api/routers/user";
+import type { inferRouterOutputs } from "@trpc/server";
+import type { AppRouter } from "@/server/api/root";
+
+type RouterOutput = inferRouterOutputs<AppRouter>;
 
 export type User = z.infer<typeof userSchema>;
+export type Lecture = RouterOutput["lecture"]["getLectures"][number];
