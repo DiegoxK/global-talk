34abb5e479129113e1957e5d942e460ed2bfe952
DiegoxK <diego.stv@outlook.es>
2024-07-30 18:47:19 -0500

    Add number type to zod import in definitions.ts

--- src/lib/definitions.ts
+++ src/lib/definitions.ts
@@ -1,4 +1,4 @@
-import type { z } from "zod";
+import type { number, z } from "zod";
 import type { userSchema } from "@/server/api/routers/user";
 import type { inferRouterOutputs } from "@trpc/server";
 import type { AppRouter } from "@/server/api/root";
@@ -7,3 +7,6 @@ type RouterOutput = inferRouterOutputs<AppRouter>;
 
 export type User = z.infer<typeof userSchema>;
 export type Lecture = RouterOutput["lecture"]["getLectures"][number];
+export type Level = RouterOutput["level"]["getLevels"][number];
+
+export type CoruseInfo = RouterOutput["course"]["getCourseName"];
