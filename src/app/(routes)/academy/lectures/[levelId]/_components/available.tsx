bf9982217e4f1f38cd8c63986388444cab5ec5b7
DiegoxK <diego.stv@outlook.es>
2024-07-31 13:21:31 -0500

    Add Available component to display available lectures for a specific level

---
+++ src/app/(routes)/academy/lectures/[levelId]/_components/available.tsx
@@ -0,0 +1,28 @@
+import { api } from "@/trpc/server";
+import LectureCard from "../../../_components/ui/lecture-card";
+
+interface LecturesProps {
+  levelId: string;
+}
+
+export default async function Available({ levelId }: LecturesProps) {
+  const lectures = await api.lecture.getLectures({
+    levelId,
+  });
+
+  return (
+    <div className="grid h-fit grid-cols-1 gap-3 pb-4 pt-4 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
+      {lectures.map(async (lecture) => {
+        const scheduleNumber = lecture.schedules.length;
+
+        return (
+          <LectureCard
+            key={lecture.id}
+            lecture={lecture}
+            scheduleCount={scheduleNumber}
+          />
+        );
+      })}
+    </div>
+  );
+}
