0136528c24eac864851019db00571d508979cdae
DiegoxK <diego.stv@outlook.es>
2024-07-30 19:16:59 -0500

    Add LevelLink component for academy navigation

---
+++ src/app/(routes)/academy/_components/navigation/level-link.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import type { Level } from "@/lib/definitions";
+import { cn } from "@/lib/utils";
+import { Sparkles } from "lucide-react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+export default function LevelLink({ level }: { level: Level }) {
+  const path = usePathname();
+  return (
+    <Link
+      className={cn(
+        "flex w-full items-center gap-2 rounded-md bg-primary-400 px-4 py-2 text-white",
+        path.includes(level.id) && "bg-primary-700",
+      )}
+      href={`/academy/lectures/${level.id}`}
+    >
+      <Sparkles className="h-5 w-5" />
+      {level.name}
+    </Link>
+  );
+}
