02e3db8ae143210321a16d13bda4537adbb8beed
DiegoxK <diego.stv@outlook.es>
2024-07-31 13:21:04 -0500

    Update lectureRouter to fetch available lectures for a specific level

--- src/server/api/routers/lectures.ts
+++ src/server/api/routers/lectures.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
-import { lectures } from "@/server/db/schema";
+import { lectures, schedules } from "@/server/db/schema";
 import { createTRPCRouter, protectedProcedure } from "../trpc";
-import { eq } from "drizzle-orm";
+import { and, eq, ne } from "drizzle-orm";
 
 export const lectureRouter = createTRPCRouter({
   getLectures: protectedProcedure
@@ -10,13 +10,30 @@ export const lectureRouter = createTRPCRouter({
         levelId: z.string().uuid(),
       }),
     )
-    .query(({ input, ctx }) => {
-      return ctx.db.query.lectures.findMany({
+    .query(async ({ input, ctx }) => {
+      const user = ctx.session.user;
+
+      //TODO: ommit teacher email
+      const levelLectures = await ctx.db.query.lectures.findMany({
         with: {
+          schedules: true,
           level: true,
           teacher: true,
         },
-        where: eq(lectures.levelId, input.levelId),
+        where: and(eq(lectures.levelId, input.levelId)),
       });
+
+      const availableLectures = levelLectures.filter((lecture) => {
+        if (lecture.schedules.length === 0) {
+          return true;
+        }
+        return lecture.schedules.some((schedule) => {
+          if (schedule.studentId === user.id) {
+            return false;
+          }
+        });
+      });
+
+      return availableLectures;
     }),
 });
