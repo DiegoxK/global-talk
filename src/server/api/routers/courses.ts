3ed69de3bd8e0821a5d792a42bdecc5e401ba4ef
DiegoxK <diego.stv@outlook.es>
2024-08-01 16:01:39 -0500

    Added publicProcedure to getCourses and getCoursesIds in courseRouter

--- src/server/api/routers/courses.ts
+++ src/server/api/routers/courses.ts
@@ -1,9 +1,22 @@
 import { z } from "zod";
 import { courses } from "@/server/db/schema";
-import { createTRPCRouter, protectedProcedure } from "../trpc";
+import { createTRPCRouter, protectedProcedure, publicProcedure } from "../trpc";
 import { eq } from "drizzle-orm";
 
 export const courseRouter = createTRPCRouter({
+  getCourses: publicProcedure.query(({ ctx }) => {
+    return ctx.db.query.courses.findMany();
+  }),
+
+  getCoursesIds: protectedProcedure.query(({ ctx }) => {
+    return ctx.db
+      .select({
+        value: courses.id,
+        label: courses.name,
+      })
+      .from(courses);
+  }),
+
   getCourseName: protectedProcedure.query(({ ctx }) => {
     const user = ctx.session.user;
 
