e2ac3400da0ba76ddf4f971a27676262655ff072
DiegoxK <diego.stv@outlook.es>
2024-08-01 18:51:13 -0500

    Moved page component code  to my-lectures

--- src/app/(routes)/academy/teacher/page.tsx
+++ src/app/(routes)/academy/teacher/page.tsx
@@ -8,24 +8,7 @@ export default async function Teacher() {
 
   return (
     <div className="flex w-full flex-col">
-      <div className="mt-4">
-        <h1 className="text-xl font-bold text-primary">Crear clase</h1>
-        <Separator />
-        <div
-          style={{
-            backgroundSize: "100px",
-          }}
-          className="bg-pattern mt-4 flex w-fit items-center justify-center rounded-lg px-16 py-24 text-white shadow-md"
-        >
-          <Plus className="h-16 w-16" />
-        </div>
-      </div>
-
-      <div className="mt-4">
-        <h1 className="text-xl font-bold text-primary">Mis clases</h1>
-        <Separator />
-        <MyLectures lectures={myLectures} />
-      </div>
+      <MyLectures lectures={myLectures} />
     </div>
   );
 }
