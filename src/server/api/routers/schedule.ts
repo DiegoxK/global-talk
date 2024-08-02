587ca67bd3e9f4f006a9a764cf186e75ca776bd9
DiegoxK <diego.stv@outlook.es>
2024-08-02 10:37:39 -0500

    Remove unused code from scheduleRouter

--- src/server/api/routers/schedule.ts
+++ src/server/api/routers/schedule.ts
@@ -3,21 +3,4 @@ import { createTRPCRouter, protectedProcedure } from "../trpc";
 import { count, eq } from "drizzle-orm";
 import { z } from "zod";
 
-export const scheduleRouter = createTRPCRouter({
-  getScheduledLectures: protectedProcedure.query(async ({ ctx }) => {
-    const user = ctx.session.user;
-
-    return ctx.db.query.schedules.findMany({
-      with: {
-        lecture: {
-          with: {
-            schedules: true,
-            level: true,
-            teacher: true,
-          },
-        },
-      },
-      where: eq(schedules.studentId, user.id),
-    });
-  }),
-});
+export const scheduleRouter = createTRPCRouter({});
