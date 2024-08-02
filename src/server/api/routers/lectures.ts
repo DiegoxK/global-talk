b43ddc567f2c09259986c9f51b2223c850dfcf39
DiegoxK <diego.stv@outlook.es>
2024-08-02 10:38:07 -0500

    Refactor lecture querys

--- src/server/api/routers/lectures.ts
+++ src/server/api/routers/lectures.ts
@@ -1,65 +1,81 @@
 import { z } from "zod";
-import { lectures } from "@/server/db/schema";
+import { lectures, levels, schedules, users } from "@/server/db/schema";
 import { createTRPCRouter, protectedProcedure } from "../trpc";
-import { eq } from "drizzle-orm";
+import { and, count, eq, ne, sql } from "drizzle-orm";
 import { createInsertSchema } from "drizzle-zod";
 import { env } from "@/env";
+import { db } from "@/server/db";
 
 const createLectureSchema = createInsertSchema(lectures, {
   teacherId: z.undefined(),
 });
 
+// Obtain a table that gets the schedulesCount to each lecture
+const sq = db
+  .select({
+    lectureId: lectures.id,
+    schedulesCount: count(schedules.id).as("schedule_count"),
+  })
+  .from(lectures)
+  .leftJoin(schedules, eq(lectures.id, schedules.lectureId))
+  .groupBy(lectures.id)
+  .as("sq");
+
+const getLecturesQuery = db
+  .select({
+    id: lectures.id,
+    name: lectures.name,
+    description: lectures.description,
+    meetUrl: lectures.meet_url,
+    off2classUrl: lectures.off2class_url,
+    date: lectures.date,
+    startTime: lectures.start_time,
+    endTime: lectures.end_time,
+    isFinished: lectures.finished,
+    teacherName: sql<string>`concat(${users.name}, ' ', ${users.lastName})`.as(
+      "teacher_name",
+    ),
+    teacherImage: users.image,
+    levelName: levels.name,
+    schedulesCount: sq.schedulesCount,
+  })
+  .from(lectures)
+  .leftJoin(sq, eq(lectures.id, sq.lectureId))
+  .leftJoin(users, eq(lectures.teacherId, users.id))
+  .leftJoin(levels, eq(lectures.levelId, levels.id));
+
 export const lectureRouter = createTRPCRouter({
-  getLectures: protectedProcedure
+  getAvailableLectures: protectedProcedure
     .input(
       z.object({
         levelId: z.string().uuid(),
       }),
     )
-    .query(async ({ input, ctx }) => {
+    .query(({ input, ctx }) => {
       const user = ctx.session.user;
 
-      //TODO: ommit teacher email
-      const levelLectures = await ctx.db.query.lectures.findMany({
-        with: {
-          schedules: true,
-          level: true,
-          teacher: true,
-        },
-        where: eq(lectures.levelId, input.levelId),
-      });
+      return getLecturesQuery.where(
+        and(eq(lectures.levelId, input.levelId), ne(users.id, user.id)),
+      );
+    }),
 
-      const availableLectures = levelLectures.filter((lecture) => {
-        if (lecture.schedules.length === 0) {
-          return true;
-        }
-        return lecture.schedules.some((schedule) => {
-          if (schedule.studentId === user.id) {
-            return false;
-          }
-        });
-      });
+  getScheduledLectures: protectedProcedure
+    .input(
+      z.object({
+        levelId: z.string().uuid(),
+      }),
+    )
+    .query(({ input, ctx }) => {
+      const user = ctx.session.user;
 
-      return availableLectures;
+      return getLecturesQuery.where(
+        and(eq(lectures.levelId, input.levelId), eq(users.id, user.id)),
+      );
     }),
 
   getMyLectures: protectedProcedure.query(async ({ ctx }) => {
     const user = ctx.session.user;
-
-    const myLectures = await ctx.db.query.lectures.findMany({
-      with: {
-        schedules: true,
-        level: {
-          with: {
-            course: true,
-          },
-        },
-        teacher: true,
-      },
-      where: eq(lectures.teacherId, user.id),
-    });
-
-    return myLectures;
+    return getLecturesQuery.where(eq(lectures.teacherId, user.id));
   }),
 
   editLecture: protectedProcedure
