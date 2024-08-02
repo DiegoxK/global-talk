85baa873d5485e14aab65467cdf5015eaa7f4001
DiegoxK <diego.stv@outlook.es>
2024-08-02 10:39:50 -0500

    Removed imports

--- src/app/(routes)/academy/teacher/page.tsx
+++ src/app/(routes)/academy/teacher/page.tsx
@@ -1,6 +1,5 @@
-import { Separator } from "@/components/ui/separator";
 import { api } from "@/trpc/server";
-import { Plus } from "lucide-react";
+
 import MyLectures from "./_components/my-lectures";
 
 export default async function Teacher() {
