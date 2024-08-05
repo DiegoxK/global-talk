7743f9bef0e98960b43caed91115746839b29274
DiegoxK <diego.stv@outlook.es>
2024-08-05 11:57:12 -0500

    Added createSchedule function to LectureInformation component

--- src/app/(routes)/academy/lectures/[levelId]/_components/lecture-information.tsx
+++ src/app/(routes)/academy/lectures/[levelId]/_components/lecture-information.tsx
@@ -5,17 +5,21 @@ import {
   Dialog,
   DialogContent,
   DialogDescription,
+  DialogFooter,
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog";
 import { Separator } from "@/components/ui/separator";
 import type { Lecture } from "@/lib/definitions";
-import { formatDate, formatTime } from "@/lib/utils";
+import { cn, formatDate, formatTime } from "@/lib/utils";
+import { api } from "@/trpc/react";
 import { FileSymlink, SquareArrowOutUpRight } from "lucide-react";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 import type { Dispatch, SetStateAction } from "react";
 
 interface LectureInformationProps {
+  state: "view" | "schedule";
   open: boolean;
   lecture: Lecture;
   setLecture: Dispatch<SetStateAction<Lecture | undefined>>;
@@ -23,11 +27,21 @@ interface LectureInformationProps {
 }
 
 export default function LectureInformation({
+  state,
   open,
   lecture,
   setLecture,
   setOpen,
 }: LectureInformationProps) {
+  const router = useRouter();
+  const { mutate: createSchedule } = api.schedule.createSchedule.useMutation();
+
+  const createScheduleHandler = () => {
+    createSchedule({ lectureId: lecture.id });
+    router.refresh();
+    setOpen(false);
+  };
+
   return (
     <Dialog
       open={open}
@@ -41,64 +55,86 @@ export default function LectureInformation({
         setOpen(open);
       }}
     >
-      <DialogContent className="flex max-h-[90vh] flex-col items-center text-center">
+      <DialogContent
+        className={cn(
+          "max-h-[90vh]",
+          state === "view" && "flex flex-col items-center text-center",
+        )}
+      >
         <DialogHeader>
-          <DialogTitle className="text-center">{lecture.name}</DialogTitle>
-          <DialogDescription className="text-center">
+          <DialogTitle className={cn(state === "view" && "text-center")}>
+            {lecture.name}
+          </DialogTitle>
+          <DialogDescription className={cn(state === "view" && "text-center")}>
             {lecture.description}
           </DialogDescription>
         </DialogHeader>
-        <div className="max-h-[70vh] w-full space-y-4 overflow-y-auto p-1">
-          <div>
-            <p className="text-primary-700">Profesor</p>
-            <p>{lecture.teacherName}</p>
-          </div>
-          <Separator />
+        {state === "view" ? (
+          <div className="max-h-[70vh] w-full space-y-4 overflow-y-auto p-1">
+            <div>
+              <p className="text-primary-700">Profesor</p>
+              <p>{lecture.teacherName}</p>
+            </div>
+            <Separator />
 
-          <div>
-            <p className="text-primary-700">Fecha</p>
-            <p>{formatDate(lecture.date)}</p>
-          </div>
-          <Separator />
+            <div>
+              <p className="text-primary-700">Fecha</p>
+              <p>{formatDate(lecture.date)}</p>
+            </div>
+            <Separator />
 
-          <div className="flex justify-center space-x-4">
-            <div>
-              <p className="text-primary-700">Hora de inicio</p>
-              <p> {formatTime(lecture.startTime)}</p>
+            <div className="flex justify-center space-x-4">
+              <div>
+                <p className="text-primary-700">Hora de inicio</p>
+                <p> {formatTime(lecture.startTime)}</p>
+              </div>
+              <div>
+                <p className="text-primary-700">Hora de fin</p>
+                <p>{formatTime(lecture.endTime)}</p>
+              </div>
             </div>
-            <div>
-              <p className="text-primary-700">Hora de fin</p>
-              <p>{formatTime(lecture.endTime)}</p>
+            <Separator />
+            <div className="flex justify-center space-x-4">
+              <div>
+                <p className="text-primary-700">Ingresar</p>
+                <Link
+                  href={lecture.meetUrl}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <Button className="mt-1" size="icon">
+                    <SquareArrowOutUpRight className="h-5 w-5" />
+                  </Button>
+                </Link>
+              </div>
+              <div>
+                <p className="text-primary-700">Material</p>
+                <Link
+                  href={lecture.off2classUrl}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <Button className="mt-1" size="icon">
+                    <FileSymlink className="h-5 w-5" />
+                  </Button>
+                </Link>
+              </div>
             </div>
           </div>
-          <Separator />
-          <div className="flex justify-center space-x-4">
+        ) : (
+          <>
             <div>
-              <p className="text-primary-700">Ingresar</p>
-              <Link
-                href={lecture.meetUrl}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <Button className="mt-1" size="icon">
-                  <SquareArrowOutUpRight className="h-5 w-5" />
-                </Button>
-              </Link>
+              Quieres agendar la clase{" "}
+              <span className="font-bold text-primary-600">{lecture.name}</span>
+              ?
             </div>
-            <div>
-              <p className="text-primary-700">Material</p>
-              <Link
-                href={lecture.off2classUrl}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <Button className="mt-1" size="icon">
-                  <FileSymlink className="h-5 w-5" />
-                </Button>
-              </Link>
-            </div>
-          </div>
-        </div>
+            <DialogFooter>
+              <Button className="rounded-sm" onClick={createScheduleHandler}>
+                Agendar
+              </Button>
+            </DialogFooter>
+          </>
+        )}
       </DialogContent>
     </Dialog>
   );
