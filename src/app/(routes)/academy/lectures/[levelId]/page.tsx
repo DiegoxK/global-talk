fb6d5c9666d69ecfa961307b64bf457d187f1f40
DiegoxK <diego.stv@outlook.es>
2024-08-05 09:23:28 -0500

    Refactor Lectures component to use async/await for fetching available and scheduled lectures

--- src/app/(routes)/academy/lectures/[levelId]/page.tsx
+++ src/app/(routes)/academy/lectures/[levelId]/page.tsx
@@ -1,24 +1,22 @@
-import { Separator } from "@/components/ui/separator";
-import Available from "./_components/available";
-import Scheduled from "./_components/scheduled";
+import { api } from "@/trpc/server";
+import MyLectures from "./_components/my-lectures";
 
 interface LecturesProps {
   params: { levelId: string };
 }
 
-export default function Lectures({ params: { levelId } }: LecturesProps) {
+export default async function Lectures({ params: { levelId } }: LecturesProps) {
+  const [availableLectures, scheduledLectures] = await Promise.all([
+    api.lecture.getAvailableLectures({ levelId }),
+    api.lecture.getScheduledLectures({ levelId }),
+  ]);
+
   return (
     <div className="flex w-full flex-col">
-      <div className="mt-4">
-        <h1 className="text-xl font-bold text-primary">Clases agendadas</h1>
-        <Separator />
-        <Scheduled levelId={levelId} />
-      </div>
-      <div className="mt-4">
-        <h1 className="text-xl font-bold text-primary">Clases disponibles</h1>
-        <Separator />
-        <Available levelId={levelId} />
-      </div>
+      <MyLectures
+        availableLectures={availableLectures}
+        scheduledLectures={scheduledLectures}
+      />
     </div>
   );
 }
