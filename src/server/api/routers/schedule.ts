6e7f240c723598637fa470539351fc4b4f6401fb
DiegoxK <diego.stv@outlook.es>
2024-08-05 11:56:50 -0500

    Added createSchedule mutation

--- src/server/api/routers/schedule.ts
+++ src/server/api/routers/schedule.ts
@@ -3,4 +3,19 @@ import { createTRPCRouter, protectedProcedure } from "../trpc";
 import { count, eq } from "drizzle-orm";
 import { z } from "zod";
 
-export const scheduleRouter = createTRPCRouter({});
+export const scheduleRouter = createTRPCRouter({
+  createSchedule: protectedProcedure
+    .input(
+      z.object({
+        lectureId: z.string().uuid(),
+      }),
+    )
+    .mutation(async ({ input, ctx }) => {
+      const user = ctx.session.user;
+
+      await ctx.db.insert(schedules).values({
+        lectureId: input.lectureId,
+        studentId: user.id,
+      });
+    }),
+});
