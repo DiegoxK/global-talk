04d98b867a503ad470f16a0560d17cbbbb0220e0
DiegoxK <diego.stv@outlook.es>
2024-08-06 17:26:39 -0500

    Refactor definitions.ts to include UserWithRole type

--- src/lib/definitions.ts
+++ src/lib/definitions.ts
@@ -1,4 +1,4 @@
-import type { number, z } from "zod";
+import type { z } from "zod";
 import type { userSchema } from "@/server/api/routers/user";
 import type { inferRouterOutputs } from "@trpc/server";
 import type { AppRouter } from "@/server/api/root";
@@ -8,6 +8,7 @@ type RouterOutput = inferRouterOutputs<AppRouter>;
 export type User = z.infer<typeof userSchema>;
 
 // Api calls return output types
+export type UserWithRole = RouterOutput["user"]["getAllUsers"][number];
 export type Lecture = RouterOutput["lecture"]["getAvailableLectures"][number];
 export type TeacherLecture =
   RouterOutput["lecture"]["getMyTeacherLectures"][number];
