7d7ca861e06b37f326ca0a12d03aa9200921a192
DiegoxK <diego.stv@outlook.es>
2024-08-06 12:17:08 -0500

    Refactor getMyLectures to getMyTeacherLectures in Teacher page

--- src/app/(routes)/academy/teacher/page.tsx
+++ src/app/(routes)/academy/teacher/page.tsx
@@ -3,11 +3,11 @@ import { api } from "@/trpc/server";
 import MyLectures from "./_components/my-lectures";
 
 export default async function Teacher() {
-  const myLectures = await api.lecture.getMyLectures();
+  const myTeacherLectures = await api.lecture.getMyTeacherLectures();
 
   return (
     <div className="flex w-full flex-col">
-      <MyLectures lectures={myLectures} />
+      <MyLectures lectures={myTeacherLectures} />
     </div>
   );
 }
