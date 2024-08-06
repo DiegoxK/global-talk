4c5150e2332f88966867d6659fbce7b531199727
DiegoxK <diego.stv@outlook.es>
2024-08-06 16:46:56 -0500

    Added management components for users, prompts, and transactions

--- src/app/(routes)/academy/admin/page.tsx
+++ src/app/(routes)/academy/admin/page.tsx
@@ -1,3 +1,28 @@
+import { Separator } from "@/components/ui/separator";
+import Users from "./sections/users";
+import Prompts from "./sections/prompts";
+import Transactions from "./sections/transactions";
+
 export default function Admin() {
-  return <div>Admin</div>;
+  return (
+    <>
+      <div className="mt-4">
+        <h1 className="text-xl font-bold text-primary">Gestion de usuarios</h1>
+        <Separator />
+        <Users />
+      </div>
+      <div className="mt-4">
+        <h1 className="text-xl font-bold text-primary">Gestion de prompts</h1>
+        <Separator />
+        <Prompts />
+      </div>
+      <div className="mt-4">
+        <h1 className="text-xl font-bold text-primary">
+          Gestion de transacciones
+        </h1>
+        <Separator />
+        <Transactions />
+      </div>
+    </>
+  );
 }
