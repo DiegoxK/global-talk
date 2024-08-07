dd3c180b0636bff4ba3ba24c94c26a0bdd229b96
DiegoxK <diego.stv@outlook.es>
2024-08-07 08:57:37 -0500

    Add vision section to about page

---
+++ src/app/(routes)/(landing)/about/sections/vision.tsx
@@ -0,0 +1,36 @@
+import Vision from "../../../../../../public/about/vision.png";
+
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+
+export default function VisionSection() {
+  return (
+    <section className="my-24 flex items-center justify-center gap-10">
+      <div>
+        <Image src={Vision} alt="Mision" />
+      </div>
+      <div className="w-[615px] space-y-6">
+        <div className="space-y-4 pr-8">
+          <h1 className="text-xl uppercase text-primary">NUESTRA VISIÓN</h1>
+          <h2 className="text-4xl font-bold text-primary">
+            Ser posicionados como la principal academia virtual en Medellín y
+            Colombia para el aprendizaje del inglés
+          </h2>
+          <p>
+            Ser la academia virtual de referencia en Medellín y Colombia para la
+            enseñanza del inglés, reconocida por nuestra excelencia educativa y
+            por formar profesionales y estudiantes con habilidades lingüísticas
+            que les permitan acceder a oportunidades laborales internacionales.
+            Aspiramos a contribuir al desarrollo de una comunidad bilingüe que
+            impulse el crecimiento económico y social de nuestra región.
+          </p>
+        </div>
+        <div className="space-x-4">
+          <Button size="lg" className="rounded-md text-lg">
+            CONTÁCTANOS
+          </Button>
+        </div>
+      </div>
+    </section>
+  );
+}
