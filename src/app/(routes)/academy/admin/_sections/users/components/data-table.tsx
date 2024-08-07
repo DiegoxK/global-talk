228b0aa48e405ddd8d5ae639b8cd7cbb8198251d
DiegoxK <diego.stv@outlook.es>
2024-08-07 07:15:12 -0500

    Refactor data table component for user management

--- src/app/(routes)/academy/admin/_sections/users/components/data-table.tsx
+++ src/app/(routes)/academy/admin/_sections/users/components/data-table.tsx
@@ -33,13 +33,18 @@ export function DataTable<TData, TValue>({
 
   return (
     <div className="rounded-md border">
-      <Table>
+      <Table className="table-fixed" style={{ minWidth: table.getTotalSize() }}>
         <TableHeader>
           {table.getHeaderGroups().map((headerGroup) => (
             <TableRow key={headerGroup.id}>
               {headerGroup.headers.map((header) => {
                 return (
-                  <TableHead key={header.id}>
+                  <TableHead
+                    key={header.id}
+                    style={{
+                      width: header.getSize(),
+                    }}
+                  >
                     {header.isPlaceholder
                       ? null
                       : flexRender(
@@ -60,7 +65,7 @@ export function DataTable<TData, TValue>({
                 data-state={row.getIsSelected() && "selected"}
               >
                 {row.getVisibleCells().map((cell) => (
-                  <TableCell key={cell.id}>
+                  <TableCell className="truncate" key={cell.id}>
                     {flexRender(cell.column.columnDef.cell, cell.getContext())}
                   </TableCell>
                 ))}
