03a0632eb31249cc74eb6f991f7e3abf2310b9c9
DiegoxK <diego.stv@outlook.es>
2024-08-07 09:21:01 -0500

    Update FAQ section styling and content

--- src/app/(routes)/(landing)/(home)/sections/questions.tsx
+++ src/app/(routes)/(landing)/(home)/sections/questions.tsx
@@ -15,39 +15,49 @@ export default function Questions() {
     },
     {
       question: "¿Cómo funciona el programa De A2 a B1?",
-      answer: "Consta de 5 niveles, con una duración de 1 año y una intensidad de 4 horas semanales.",
+      answer:
+        "Consta de 5 niveles, con una duración de 1 año y una intensidad de 4 horas semanales.",
     },
     {
       question: "¿Cómo funciona el programa De B1 a B2?",
-      answer: "Consta de 5 niveles, con una duración de 1 año y una intensidad de 4 horas semanales.",
+      answer:
+        "Consta de 5 niveles, con una duración de 1 año y una intensidad de 4 horas semanales.",
     },
     {
       question: '¿Cómo funciona el programa "Inglés para los negocios"?',
-      answer: "Consta de 5 niveles, con una duración de 1 año y una intensidad de 4 horas semanales. \n Prerequisito: B1",
+      answer:
+        "Consta de 5 niveles, con una duración de 1 año y una intensidad de 4 horas semanales. \n Prerequisito: B1",
     },
     {
-      question: '¿Cómo funciona el programa "Preparación para exámenes oficiales"?',
-      answer: "Te preparamos para los siguientes exámenes: TOEFL, IELTS, APTIS. \n Cursos con intensidad según tus necesidades y disponibilidad. \n Simulador del examen.",
+      question:
+        '¿Cómo funciona el programa "Preparación para exámenes oficiales"?',
+      answer:
+        "Te preparamos para los siguientes exámenes: TOEFL, IELTS, APTIS. \n Cursos con intensidad según tus necesidades y disponibilidad. \n Simulador del examen.",
     },
     {
       question: "¿Tengo que estar en Medellín?",
-      answer: "No, nuestros cursos son virtuales por lo tanto no es necesario que vivas en nuestra ciudad para aprender con Global Talk Medallo.",
+      answer:
+        "No, nuestros cursos son virtuales por lo tanto no es necesario que vivas en nuestra ciudad para aprender con Global Talk Medallo.",
     },
     {
       question: "¿Qué sucede si cuento con conocimientos previos?",
-      answer: "Dentro de nuestra academia, podrás realizar un exámen de nivelación completo para reconocer tu nivel actual. En base a tus resultados te recomendaremos el programa que más se ajuste a tus conocimientos.",
+      answer:
+        "Dentro de nuestra academia, podrás realizar un exámen de nivelación completo para reconocer tu nivel actual. En base a tus resultados te recomendaremos el programa que más se ajuste a tus conocimientos.",
     },
     {
       question: "¿Cómo sé si hay cupos disponibles?",
-      answer: "Dentro de nuestra sección academia podrás ver los cupos disponibles en diferentes grupos. \n Cada grupo de compone de 5 estudiantes.",
+      answer:
+        "Dentro de nuestra sección academia podrás ver los cupos disponibles en diferentes grupos. \n Cada grupo de compone de 5 estudiantes.",
     },
     {
       question: "¿Qué plataformas usan para enseñar?",
-      answer: "Utilizamos diferentes plataformas para darte la mejor experiencia de aprendizaje, algunas de estas son: \n Google Meet \n Off 2 Class \n Write and Improve \n Globi, ¡nuestra propia IA!",
+      answer:
+        "Utilizamos diferentes plataformas para darte la mejor experiencia de aprendizaje, algunas de estas son: \n Google Meet \n Off 2 Class \n Write and Improve \n Globi, ¡nuestra propia IA!",
     },
     {
       question: "¿De qué tamaño son los grupos?",
-      answer: "Cada grupo se compone de 5 estudiantes, con el fin de brindarte una enseñanza completamente personalizada.",
+      answer:
+        "Cada grupo se compone de 5 estudiantes, con el fin de brindarte una enseñanza completamente personalizada.",
     },
     {
       question: "¿Dan clases privadas?",
@@ -55,10 +65,10 @@ export default function Questions() {
     },
     {
       question: "¿En cuanto tiempo se llega a B2?",
-      answer: "Depende de tu punto de partida. Por ejemplo, si inicias sin conocimientos previos, llegarás a B2 en 2 años con la intensidad de 4 horas semanales.",
+      answer:
+        "Depende de tu punto de partida. Por ejemplo, si inicias sin conocimientos previos, llegarás a B2 en 2 años con la intensidad de 4 horas semanales.",
     },
   ];
-  
 
   const Column = ({ position }: { position: "left" | "right" }) => {
     const columnQuestions =
@@ -83,7 +93,9 @@ export default function Questions() {
   return (
     <section id="questions" className="mb-20 mt-20 space-y-16 md:mb-40">
       <div className="flex flex-col gap-6 text-center">
-        <h2 className="text-5xl font-semibold">Preguntas frecuentes</h2>
+        <h2 className="text-5xl font-semibold text-primary">
+          Preguntas frecuentes
+        </h2>
         <p className="self-center md:w-[680px]">
           Todo lo que necesitas saber está en nuestra sección de preguntas
           frecuentes, diseñada para resolver tus dudas más comunes.
@@ -104,21 +116,46 @@ interface CardProps {
 }
 
 const Card = ({ index, question, answer }: CardProps) => {
+  const paragraphs = answer.split("\n").map((paragraph) => ({ paragraph }));
+  const Answer = () => {
+    if (paragraphs.length > 1) {
+      return (
+        <>
+          {paragraphs.map((paragraph, index) => {
+            if (index === 0) {
+              return (
+                <p key={index} className="text-justify">
+                  {paragraph.paragraph}
+                </p>
+              );
+            }
+            return (
+              <p key={index} className="ml-2 text-justify">
+                • {paragraph.paragraph}
+              </p>
+            );
+          })}
+        </>
+      );
+    }
+    return <p className="text-justify">{answer}</p>;
+  };
+
   return (
     <Accordion
-      className="border-3 rounded-lg border-primary-900 px-5 py-1"
+      className="rounded-lg border-none bg-primary px-5 py-1 transition-colors duration-200 hover:bg-primary-600"
       type="single"
       collapsible
     >
       <AccordionItem value={`item-${index}`}>
-        <AccordionTrigger className="text-start font-semibold">
+        <AccordionTrigger className="text-start font-semibold text-white">
           {question}
-          <div className="rounded-sm border border-primary-900 bg-primary-800 p-1 [&[data-state=open]>svg]:rotate-180">
-            <ChevronDown className="h-4 w-4 shrink-0 text-white transition-transform duration-200 " />
+          <div className="rounded-sm border-none bg-white p-1 [&[data-state=open]>svg]:rotate-180">
+            <ChevronDown className="h-4 w-4 shrink-0 text-primary transition-transform duration-200" />
           </div>
         </AccordionTrigger>
-        <AccordionContent className="border-t-2 border-primary-900 pt-4">
-          {answer}
+        <AccordionContent className="mb-3 rounded-md bg-white px-4 py-3">
+          <Answer />
         </AccordionContent>
       </AccordionItem>
     </Accordion>
