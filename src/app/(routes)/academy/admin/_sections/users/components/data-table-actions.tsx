2b7ff70d9343d26e6a7c9d4980c8277489a0ae70
DiegoxK <diego.stv@outlook.es>
2024-08-07 08:19:05 -0500

    Added DataTableActions component for user management

---
+++ src/app/(routes)/academy/admin/_sections/users/components/data-table-actions.tsx
@@ -0,0 +1,37 @@
+import type { UserWithRole } from "@/lib/definitions";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { MoreHorizontal } from "lucide-react";
+
+interface DataTableActionsProps {
+  user: UserWithRole;
+}
+
+export default function DataTableActions({ user }: DataTableActionsProps) {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" className="h-8 w-8 p-0">
+          <span className="sr-only">Abrir menu</span>
+          <MoreHorizontal className="h-4 w-4" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuLabel>Acciones</DropdownMenuLabel>
+        <DropdownMenuItem
+          onClick={() => navigator.clipboard.writeText(user.name)}
+        >
+          Nombre
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
