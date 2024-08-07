7fe120ac275fc479714cfd9c1fdd480639cf1873
DiegoxK <diego.stv@outlook.es>
2024-08-07 08:18:44 -0500

    Remove prompts section from admin page

--- src/app/(routes)/academy/admin/page.tsx
+++ src/app/(routes)/academy/admin/page.tsx
@@ -1,6 +1,5 @@
 import { Separator } from "@/components/ui/separator";
 import Users from "./_sections/users";
-import Prompts from "./_sections/prompts";
 import Transactions from "./_sections/transactions";
 
 export default function Admin() {
@@ -10,11 +9,6 @@ export default function Admin() {
         <h1 className="text-xl font-bold text-primary">Gestion de usuarios</h1>
         <Separator />
         <Users />
-      </div>
-      <div className="mt-4">
-        <h1 className="text-xl font-bold text-primary">Gestion de prompts</h1>
-        <Separator />
-        <Prompts />
       </div>
       <div className="mt-4">
         <h1 className="text-xl font-bold text-primary">
