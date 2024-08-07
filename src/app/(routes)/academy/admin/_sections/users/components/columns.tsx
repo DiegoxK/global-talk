b7a97cd84cd2bd9f165df5ef6a8b0990a29f563e
DiegoxK <diego.stv@outlook.es>
2024-08-07 07:15:19 -0500

    Add avatar column to user management data table

--- src/app/(routes)/academy/admin/_sections/users/components/columns.tsx
+++ src/app/(routes)/academy/admin/_sections/users/components/columns.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
 import type { UserWithRole } from "@/lib/definitions";
 import type { ColumnDef } from "@tanstack/react-table";
 
@@ -7,17 +9,30 @@ export const columns: ColumnDef<UserWithRole>[] = [
   {
     accessorKey: "image",
     header: "Imagen",
+    size: 60,
+    cell: ({ row }) => (
+      <Avatar>
+        <AvatarImage src={row.original.image ?? undefined} />
+        <AvatarFallback>
+          {row.original.name[0]}
+          {row.original.lastName[0]}
+        </AvatarFallback>
+      </Avatar>
+    ),
   },
   {
     accessorKey: "name",
     header: "Nombre",
+    size: 70,
   },
   {
     accessorKey: "lastName",
     header: "Apellido",
+    size: 100,
   },
   {
     accessorKey: "email",
+    size: 200,
     header: "Email",
   },
   {
