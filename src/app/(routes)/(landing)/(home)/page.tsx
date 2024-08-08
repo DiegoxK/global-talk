4c2c93f6fc90853ddd3989c10abed2eed91432be
DiegoxK <diego.stv@outlook.es>
2024-08-08 10:17:05 -0500

    Added courses section to home page

--- src/app/(routes)/(landing)/(home)/page.tsx
+++ src/app/(routes)/(landing)/(home)/page.tsx
@@ -1,3 +1,4 @@
+import Courses from "./sections/courses";
 import Hero from "./sections/hero";
 import Qualities from "./sections/qualities";
 import Questions from "./sections/questions";
@@ -8,6 +9,7 @@ export default async function Home() {
   return (
     <>
       <Hero />
+      <Courses />
       <Qualities />
       <Road />
       <Team />
