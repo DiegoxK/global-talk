1f9c4b8708ecf2cdccd1ee4a9f0bd17371019093
DiegoxK <diego.stv@outlook.es>
2024-08-08 11:15:18 -0500

    Moved team section to about

--- src/app/(routes)/(landing)/about/sections/team.tsx
+++ src/app/(routes)/(landing)/about/sections/team.tsx
@@ -53,24 +53,6 @@ export default function Team() {
           />
         ))}
       </div>
-      <div className="hidden items-center justify-between rounded-xl bg-primary-700 p-10 text-white md:flex">
-        <h2 className="text-3xl">
-          ¡Transforma tu futuro con el poder del inglés!
-        </h2>
-        <Link href="/contact">
-          <Button className="ms-10 flex gap-2" variant="secondary">
-            Registrarme ahora
-            <svg
-              width="21"
-              height="15"
-              viewBox="0 0 21 15"
-              className="fill-current"
-            >
-              <path d="M20.7236 8.01862C21.1048 7.619 21.0899 6.98601 20.6903 6.6048L14.1781 0.392536C13.7785 0.0113211 13.1455 0.0262382 12.7643 0.425855C12.3831 0.825471 12.398 1.45846 12.7976 1.83968L18.5862 7.36169L13.0642 13.1502C12.683 13.5499 12.6979 14.1828 13.0975 14.5641C13.4971 14.9453 14.1301 14.9304 14.5113 14.5307L20.7236 8.01862ZM1.02356 8.77585L20.0236 8.32809L19.9764 6.32865L0.976441 6.7764L1.02356 8.77585Z" />
-            </svg>
-          </Button>
-        </Link>
-      </div>
     </section>
   );
 }
