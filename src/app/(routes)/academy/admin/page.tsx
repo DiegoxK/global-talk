979e583061f3481b951f90dd90db1f1f1f8fe8cc
DiegoxK <diego.stv@outlook.es>
2024-08-23 12:40:16 -0500

    Changed the layout to have navigation

--- src/app/(routes)/academy/admin/page.tsx
+++ src/app/(routes)/academy/admin/page.tsx
@@ -1,22 +1,23 @@
-import { Separator } from "@/components/ui/separator";
-import Users from "./_sections/users";
-import Transactions from "./_sections/transactions";
+import { Transactions, Users } from "@/vectors/miscellaneous";
+import Link from "next/link";
 
 export default function Admin() {
   return (
-    <>
-      <div className="mt-4">
-        <h1 className="text-xl font-bold text-primary">Gestion de usuarios</h1>
-        <Separator />
-        <Users />
-      </div>
-      <div className="mt-4">
-        <h1 className="text-xl font-bold text-primary">
-          Gestion de transacciones
-        </h1>
-        <Separator />
-        <Transactions />
-      </div>
-    </>
+    <div className="flex h-full flex-wrap items-center justify-center gap-x-14 gap-y-10 py-10">
+      <Link href="/academy/admin/users">
+        <button className="flex h-[410px] w-[410px] flex-col items-center justify-center rounded-md border shadow-md">
+          <Users width={280} height={280} />
+          <h2 className="text-2xl font-bold text-primary-600">Usuarios</h2>
+          <p>Gestion de usuarios</p>
+        </button>
+      </Link>
+      <Link href="/academy/admin/transactions">
+        <button className="flex h-[410px] w-[410px] flex-col items-center justify-center rounded-md border shadow-md">
+          <Transactions width={280} height={280} />
+          <h2 className="text-2xl font-bold text-primary-600">Transacciones</h2>
+          <p>Gestion de transacciones</p>
+        </button>
+      </Link>
+    </div>
   );
 }
