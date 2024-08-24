1d7b88f901721e5d0b722e0864dc386130252491
DiegoxK <diego.stv@outlook.es>
2024-08-24 10:44:45 -0500

    Added pagination and filtering to user table

--- src/app/(routes)/academy/admin/users/components/data-table.tsx
+++ src/app/(routes)/academy/admin/users/components/data-table.tsx
@@ -2,10 +2,15 @@
 
 import {
   type ColumnDef,
+  type ColumnFiltersState,
   flexRender,
   getCoreRowModel,
+  getPaginationRowModel,
+  getFilteredRowModel,
   useReactTable,
 } from "@tanstack/react-table";
+
+import { Input } from "@/components/ui/input";
 
 import {
   Table,
@@ -18,6 +23,8 @@ import {
 import DataTableActions from "./data-table-actions";
 import { useState } from "react";
 import type { UserWithRole } from "@/lib/definitions";
+import DataTableDialog from "./data-table-dialog";
+import { DataTablePagination } from "./data-table-pagination";
 
 interface DataTableProps<TData extends UserWithRole, TValue> {
   columns: ColumnDef<TData, TValue>[];
@@ -28,65 +35,94 @@ export function DataTable<TData extends UserWithRole, TValue>({
   columns,
   data,
 }: DataTableProps<TData, TValue>) {
-  const [dialogOpen, setDialogOpen] = useState(false);
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const [user, setUser] = useState<UserWithRole>();
+
+  const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
 
   const table = useReactTable({
     data,
     columns,
     getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    onColumnFiltersChange: setColumnFilters,
+    getFilteredRowModel: getFilteredRowModel(),
+    state: {
+      columnFilters,
+    },
   });
 
   return (
-    <div className="rounded-md border">
-      <Table>
-        <TableHeader>
-          {table.getHeaderGroups().map((headerGroup) => (
-            <TableRow key={headerGroup.id}>
-              {headerGroup.headers.map((header) => {
-                return (
-                  <TableHead key={header.id}>
-                    {header.isPlaceholder
-                      ? null
-                      : flexRender(
-                          header.column.columnDef.header,
-                          header.getContext(),
-                        )}
-                  </TableHead>
-                );
-              })}
+    <>
+      <DataTableDialog open={isDialogOpen} setOpen={setIsDialogOpen} />
+      <div className="flex items-center pb-4">
+        <Input
+          placeholder="Buscar por email..."
+          value={(table.getColumn("email")?.getFilterValue() as string) ?? ""}
+          onChange={(event) =>
+            table.getColumn("email")?.setFilterValue(event.target.value)
+          }
+          className="max-w-sm"
+        />
+      </div>
+      <div className="rounded-md border">
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableHead key={header.id}>
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext(),
+                          )}
+                    </TableHead>
+                  );
+                })}
 
-              <TableHead className="flex items-center justify-end">
-                Acciones
-              </TableHead>
-            </TableRow>
-          ))}
-        </TableHeader>
-        <TableBody>
-          {table.getRowModel().rows?.length ? (
-            table.getRowModel().rows.map((row) => (
-              <TableRow
-                key={row.id}
-                data-state={row.getIsSelected() && "selected"}
-              >
-                {row.getVisibleCells().map((cell) => (
-                  <TableCell className="truncate" key={cell.id}>
-                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                <TableHead className="flex items-center justify-end">
+                  Acciones
+                </TableHead>
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && "selected"}
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell className="truncate" key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext(),
+                      )}
+                    </TableCell>
+                  ))}
+                  <TableCell className="mr-3 mt-1 flex items-center justify-end">
+                    <DataTableActions user={row.original} />
                   </TableCell>
-                ))}
-                <TableCell className="mr-3 mt-1 flex items-center justify-end">
-                  <DataTableActions user={row.original} />
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell
+                  colSpan={columns.length}
+                  className="h-24 text-center"
+                >
+                  No results.
                 </TableCell>
               </TableRow>
-            ))
-          ) : (
-            <TableRow>
-              <TableCell colSpan={columns.length} className="h-24 text-center">
-                No results.
-              </TableCell>
-            </TableRow>
-          )}
-        </TableBody>
-      </Table>
-    </div>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+      <DataTablePagination table={table} />
+    </>
   );
 }
