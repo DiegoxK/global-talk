28ff1b81e064c44eeecfc9b9b4deba181b9478dd
DiegoxK <diego.stv@outlook.es>
2024-09-27 09:26:56 -0500

    Add contact form component with validation and toast notifications for user feedback on form submission

---
+++ src/app/(routes)/(landing)/contact/components/contact-form.tsx
@@ -0,0 +1,185 @@
+"use client";
+
+import { z } from "zod";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { toast, useToast } from "@/components/ui/use-toast";
+import { useForm } from "react-hook-form";
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+
+type ContactType = z.infer<typeof contactSchema>;
+
+const contactSchema = z.object({
+  name: z
+    .string()
+    .min(1, {
+      message: "El nombre es requerido",
+    })
+    .max(50, {
+      message: "El nombre debe tener menos de 50 caracteres",
+    }),
+
+  phone: z
+    .string()
+    .regex(/^\d+$/, {
+      message: "El teléfono solo puede contener números",
+    })
+    .min(1, {
+      message: "El teléfono es requerido",
+    })
+    .max(50, {
+      message: "El teléfono debe tener menos de 50 caracteres",
+    }),
+
+  email: z.string().email({
+    message: "El email no es válido",
+  }),
+
+  city: z
+    .string()
+    .min(1, {
+      message: "La ciudad es requerida",
+    })
+    .max(50, {
+      message: "La ciudad debe tener menos de 50 caracteres",
+    }),
+
+  message: z
+    .string()
+    .min(1, {
+      message: "El mensaje es requerido",
+    })
+    .max(500, {
+      message: "El mensaje debe tener menos de 500 caracteres",
+    }),
+});
+
+export default function ContactForm() {
+  const form = useForm<ContactType>({
+    resolver: zodResolver(contactSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      city: "",
+      phone: "",
+      message: "",
+    },
+  });
+
+  const onSubmit = (values: ContactType) => {
+    const toastAlert = toast({
+      title: "Enviando mensaje",
+      description: "Por favor espera un momento",
+      duration: 2000,
+    });
+
+    setTimeout(() => {
+      toastAlert.update(
+        toast({
+          title: "Atualmente en mantenimiento!",
+          description: "Por favor inténtalo más tarde.",
+          duration: 4000,
+        }),
+      );
+    }, 3000);
+  };
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="flex flex-col gap-y-2 pb-8 pt-6 lg:px-4"
+      >
+        <div className="grid grid-cols-1 gap-4 lg:grid-cols-2">
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>
+                  Tu nombre completo{" "}
+                  <span className="text-primary">
+                    <span className="text-primary">*</span>
+                  </span>
+                </FormLabel>
+                <FormControl>
+                  <Input placeholder="Juan Pérez" {...field} />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="phone"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>
+                  Tu celular <span className="text-primary">*</span>
+                </FormLabel>
+                <FormControl>
+                  <Input placeholder="321000000" {...field} />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+        </div>
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>
+                Tu correo <span className="text-primary">*</span>
+              </FormLabel>
+              <FormControl>
+                <Input placeholder="alfonso@globtm.com" {...field} />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="city"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>
+                Escoge tu Ciudad <span className="text-primary">*</span>
+              </FormLabel>
+              <FormControl>
+                <Input placeholder="Medellín" {...field} />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="message"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>
+                Mensaje <span className="text-primary">*</span>
+              </FormLabel>
+              <FormControl>
+                <Textarea placeholder="Tu mensaje" {...field} />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <Button type="submit" className="mt-4 rounded-md">
+          ¡Enviar Mensaje!
+        </Button>
+      </form>
+    </Form>
+  );
+}
