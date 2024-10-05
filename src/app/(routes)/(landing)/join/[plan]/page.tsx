221d566c64f878cae972edb6308f1071aa6e3208
DiegoxK <diego.stv@outlook.es>
2024-10-05 10:00:04 -0500

    Refactor landing page to streamline plan handling and improve code structure for better maintainability and clarity

--- src/app/(routes)/(landing)/join/[plan]/page.tsx
+++ src/app/(routes)/(landing)/join/[plan]/page.tsx
@@ -11,15 +11,22 @@ import {
 import Link from "next/link";
 
 export default function Page({ params }: { params: { plan: string } }) {
-  console.log(params.plan);
+  const planId = params.plan;
 
-  const individual = [
+  switch (planId) {
+    case "beginners_a0":
+      return <BeginnersA0 planId={planId} />;
+  }
+}
+
+const BeginnersA0 = ({ planId }: { planId: string }) => {
+  const plans = [
     {
-      href: `/checkout/${params.plan}/recurrent`,
+      href: `/checkout/${planId}/recurrent`,
       title: "Pago 2 cuotas / nivel",
       description: "Matrícula individual",
       price: "300 mil COP / mes",
-      duration: "2 mes",
+      duration: "1 mes",
       features: [
         "16 clases de una hora (4 por semana)",
         "Acceso a la plataforma de aprendizaje",
@@ -31,7 +38,7 @@ export default function Page({ params }: { params: { plan: string } }) {
       savings: "",
     },
     {
-      href: `/checkout/${params.plan}/one-time-level`,
+      href: `/checkout/${planId}/one-time-level`,
       title: "Pago único por nivel",
       description: "Matrícula individual",
       price: "530 mil COP / nivel",
@@ -48,10 +55,10 @@ export default function Page({ params }: { params: { plan: string } }) {
     },
 
     {
-      href: `/checkout/${params.plan}/one-time`,
+      href: `/checkout/${planId}/one-time`,
       title: "Programa completo",
       description: "Matrícula individual - Programa completo",
-      price: "1 millón COP",
+      price: "1 millón COP / programa",
       duration: "4 meses (2 niveles)",
       features: [
         "64 clases de una hora (4 por semana)",
@@ -65,61 +72,13 @@ export default function Page({ params }: { params: { plan: string } }) {
     },
   ];
 
-  const grupo = [
-    {
-      title: "Opción 4",
-      description: "Matrícula grupo de amigos (3-5 personas)",
-      price: "430 mil / nivel por persona",
-      duration: "2 meses",
-      features: [
-        "32 clases de una hora (4 por semana)",
-        "Acceso a la plataforma de aprendizaje para ti y tus amigos",
-        "Grabaciones de las clases",
-        "Todos los materiales",
-        "Evaluación al final del nivel",
-        "3-5 cupos en un grupo de máximo 5 personas",
-      ],
-      savings: "Ahorras 170 mil pesos por nivel por persona",
-    },
-    {
-      title: "Opción 3",
-      description: "Matrícula grupo de amigos (3-5 personas)",
-      price: "250 mil / mes por persona",
-      duration: "4 meses",
-      features: [
-        "16 clases de una hora (4 por semana)",
-        "Acceso a la plataforma de aprendizaje para ti y tus amigos",
-        "Grabaciones de las clases",
-        "Todos los materiales",
-        "Evaluación al final del nivel",
-        "3-5 cupos en un grupo de máximo 5 personas",
-      ],
-      savings: "Ahorras 50 mil pesos al mes por persona",
-    },
-    {
-      title: "Opción 6",
-      description: "Matrícula grupo de amigos - Programa completo",
-      price: "850 mil",
-      duration: "4 meses (2 niveles)",
-      features: [
-        "64 clases de una hora (4 por semana)",
-        "Acceso a la plataforma de aprendizaje para ti y tus amigos",
-        "Grabaciones de las clases",
-        "Todos los materiales",
-        "Evaluación al final de cada nivel",
-        "3-5 cupos en un grupo de máximo 5 personas",
-      ],
-      savings: "Ahorras 350 mil pesos por persona",
-    },
-  ];
-
   return (
     <div className="container mx-auto flex flex-col items-center gap-12 border-t py-16">
       <h1 className="text-4xl font-bold text-primary">
         Beginners A0 - Matrícula individual
       </h1>
       <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
-        {individual.map((plan, index) => (
+        {plans.map((plan, index) => (
           <Card key={index} className="flex flex-col">
             <CardHeader>
               <CardTitle>{plan.title}</CardTitle>
@@ -154,10 +113,6 @@ export default function Page({ params }: { params: { plan: string } }) {
           </Card>
         ))}
       </div>
-      {/* <Separator />
-      <h1 className="text-4xl font-bold text-primary">
-        Beginners A0 - Matrícula grupo de amigos
-      </h1> */}
     </div>
   );
-}
+};
