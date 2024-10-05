11c1aafeb0557ac89e1759cefbf03479fb247766
DiegoxK <diego.stv@outlook.es>
2024-10-05 15:23:05 -0500

    Refactor join page component: Simplify imports by removing unnecessary elements for cleaner code and improved maintainability

--- src/app/(routes)/(landing)/join/page.tsx
+++ src/app/(routes)/(landing)/join/page.tsx
@@ -1,12 +1,4 @@
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-
-import { Separator } from "@/components/ui/separator";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { CheckIcon } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
