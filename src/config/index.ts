416abb2c58390708715ec4cbd551c4f6e66ef50a
DiegoxK <diego.stv@outlook.es>
2024-10-07 09:15:49 -0500

    Refactor site configuration: Introduce interfaces for navigation and pricing, enhance structure for clarity and maintainability

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,31 @@
-export const siteConfig = {
+interface SiteConfig {
+  navigation: Navigation[];
+  pricing: Pricing;
+}
+
+interface Navigation {
+  label: string;
+  url: string;
+  subLinks?: Navigation[];
+}
+type PricingKey =
+  | "beginners_a0"
+  | "pure_a1"
+  | "beginners_plus_pure_a1"
+  | "intensive_b2"
+  | "business"
+  | "from_a2_to_b1"
+  | "from_b1_to_b2";
+
+interface PricingDetails {
+  price: string;
+  name: string;
+  description: string;
+}
+
+export type Pricing = Record<PricingKey, PricingDetails>;
+
+export const siteConfig: SiteConfig = {
   navigation: [
     {
       label: "Inicio",
@@ -36,4 +63,42 @@ export const siteConfig = {
       subLinks: [{ label: "Formulario de contacto", url: "/contact#form" }],
     },
   ],
+
+  pricing: {
+    beginners_a0: {
+      price: "90000",
+      name: "Beginners_A0",
+      description: "Programa Beginners A0 – 2 niveles (4 meses)",
+    },
+    pure_a1: {
+      price: "90000",
+      name: "Pure_A1",
+      description: "Programa Pure A1 – 2 niveles (4 meses)",
+    },
+    beginners_plus_pure_a1: {
+      price: "90000",
+      name: "Beginners + Pure A1",
+      description: "Programa Beginners + Pure A1 – 2 niveles (4 meses)",
+    },
+    from_a2_to_b1: {
+      price: "90000",
+      name: "From A2 to B1",
+      description: "Programa From A2 to B1 – 2 niveles (4 meses)",
+    },
+    from_b1_to_b2: {
+      price: "90000",
+      name: "From B1 to B2",
+      description: "Programa From B1 to B2 – 2 niveles (4 meses)",
+    },
+    intensive_b2: {
+      price: "90000",
+      name: "Intensive B2",
+      description: "Programa Intensive B2 – 2 niveles (4 meses)",
+    },
+    business: {
+      price: "90000",
+      name: "Business English",
+      description: "Programa Business English – 2 niveles (4 meses)",
+    },
+  },
 };
