e2e90225d265954066d306ee2b3316808e4242f2
DiegoxK <diego.stv@outlook.es>
2024-10-18 12:43:59 -0500

    Add AI response endpoint to prompt router, integrating OpenAI API for chat completions and ensuring user authorization

--- src/server/api/routers/prompt.ts
+++ src/server/api/routers/prompt.ts
@@ -1,7 +1,11 @@
 import { prompts } from "@/server/db/schema";
 import { createTRPCRouter, protectedProcedure } from "../trpc";
+import OpenAI from "openai";
 import { and, count, eq } from "drizzle-orm";
 import { z } from "zod";
+import { ChatCompletionRole } from "openai/resources/index.mjs";
+
+const openai = new OpenAI();
 
 export const promptRouter = createTRPCRouter({
   getPrompts: protectedProcedure.query(({ ctx }) => {
@@ -18,4 +22,34 @@ export const promptRouter = createTRPCRouter({
       })
       .from(prompts);
   }),
+
+  getAiResponse: protectedProcedure
+    .input(
+      z.object({
+        messages: z.array(
+          z.object({
+            content: z.string(),
+            role: z.enum(["system", "user", "assistant"]),
+          }),
+        ),
+      }),
+    )
+    .query(async ({ ctx, input }) => {
+      const user = ctx.session.user;
+
+      if (!user) {
+        throw new Error("Unauthorized");
+      }
+
+      const completion = await openai.chat.completions.create({
+        model: "gpt-4o-mini",
+        messages: input.messages,
+      });
+
+      if (completion?.choices[0]?.message) {
+        return completion.choices[0].message.content;
+      }
+
+      throw new Error("Error al obtener respuesta del chat");
+    }),
 });
