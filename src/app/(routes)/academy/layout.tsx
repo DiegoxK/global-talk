6403efd890ba306c00c87a3501a8e1f3dcf8f970
DiegoxK <diego.stv@outlook.es>
2024-10-18 10:38:21 -0500

    Changed top latout color

--- src/app/(routes)/academy/layout.tsx
+++ src/app/(routes)/academy/layout.tsx
@@ -25,7 +25,7 @@ export default async function RootLayout({
       <Sidebar user={session.user} program={program} firstLevel={firstLevel} />
       <div className="mx-4 mt-4 flex w-full flex-col">
         {/* TODO: Imagen? */}
-        <div className="flex w-full items-center justify-center gap-1 rounded-lg bg-primary-700 py-10 text-3xl font-extrabold text-white">
+        <div className="flex w-full items-center justify-center gap-1 rounded-lg bg-primary py-10 text-3xl font-extrabold text-white">
           <Logo className="fill-white" height={40} width={38} />
           lobal talk medallo
         </div>
