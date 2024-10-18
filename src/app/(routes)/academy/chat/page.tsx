c05b4c80dc01c8a82c27d949a271b5c91c0a0748
DiegoxK <diego.stv@outlook.es>
2024-10-18 12:08:51 -0500

    Enhance Chat component to fetch prompts from API and integrate ChatBot for improved user interaction experience

--- src/app/(routes)/academy/chat/page.tsx
+++ src/app/(routes)/academy/chat/page.tsx
@@ -1,3 +1,12 @@
-export default function Chat() {
-  return <div>Chat</div>;
+import { api } from "@/trpc/server";
+import ChatBot from "./chat-bot";
+
+export default async function Chat() {
+  const prompts = await api.prompt.getPrompts();
+
+  return (
+    <div className="bg-pattern my-4 h-[calc(100vh-10.5rem)] rounded-sm">
+      <ChatBot prompts={prompts} />
+    </div>
+  );
 }
