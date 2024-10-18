4b484e8c4702cad8973565771cd2830719a977db
DiegoxK <diego.stv@outlook.es>
2024-10-17 19:07:52 -0500

    Update cron route: Add levels and programs to schema imports; streamline student activation logic and handle level progression more efficiently

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -1,8 +1,8 @@
 import { NextResponse } from "next/server";
-import { groups, users } from "@/server/db/schema";
+import { groups, levels, programs, users } from "@/server/db/schema";
 import { db } from "@/server/db";
 import { env } from "@/env";
-import { and, eq, gt } from "drizzle-orm";
+import { and, count, eq, gt, sql } from "drizzle-orm";
 import { chargeSubscription, getCustomerById } from "@/lib/epayco";
 
 export const maxDuration = 60;
@@ -11,71 +11,58 @@ interface Student {
   planType: "RECURRENT" | "LEVEL" | "COMPLETE" | "STAFF" | "EXTERNAL";
   id: string;
   name: string;
-  ip: string | null;
-  subscriptionId: string | null;
   programId: string;
-  customerId: string | null;
   group: {
     startingDate: string;
   };
 }
 
-async function sequentialForEach<T>(
-  array: (T | undefined)[],
-  callback: (item: T, index: number, array: (T | undefined)[]) => Promise<void>,
-): Promise<void> {
-  for (let index = 0; index < array.length; index++) {
-    const item = array[index];
-    if (item !== undefined) {
-      await callback(item, index, array);
-    }
-  }
-}
-
 function getFinishLevelDate(startDate: Date, currentLevel: number): Date {
   const resultDate = new Date(startDate);
 
-  resultDate.setDate(resultDate.getDate() + 62 * currentLevel);
+  resultDate.setDate(resultDate.getDate() + 60 * currentLevel);
 
   return resultDate;
 }
 
-const chargeStudentSubscription = async (student: Student) => {
-  if (student.customerId) {
-    console.log("getting epayco customer");
-    const epaycoCustomer = await getCustomerById(student.customerId);
-    const customerData = epaycoCustomer?.data?.data;
+// const chargeStudentSubscription = async (student: Student) => {
+//   if (student.customerId) {
+//     console.log("getting epayco customer");
+//     const epaycoCustomer = await getCustomerById(student.customerId);
+//     const customerData = epaycoCustomer?.data?.data;
 
-    console.log("cheking if customer data exists");
-    if (customerData && student.ip && customerData?.cards[0]?.token) {
-      console.log("charging subscription");
-      try {
-        const epaycoSubscriptionResponse = await chargeSubscription({
-          id_plan: student.programId,
-          customer: customerData.id_customer,
-          token_card: customerData.cards[0].token,
-          doc_type: "CC",
-          doc_number: customerData.doc_number,
-          ip: student.ip,
-        });
+//     console.log("cheking if customer data exists");
+//     if (customerData && student.ip && customerData?.cards[0]?.token) {
+//       console.log("charging subscription");
+//       try {
+//         const epaycoSubscriptionResponse = await chargeSubscription({
+//           id_plan: student.programId,
+//           customer: customerData.id_customer,
+//           token_card: customerData.cards[0].token,
+//           doc_type: "CC",
+//           doc_number: customerData.doc_number,
+//           ip: student.ip,
+//         });
 
-        console.log("subscription charged ");
-        return epaycoSubscriptionResponse;
-      } catch (error) {
-        console.error("Error charging subscription", error);
-      }
-    } else {
-      console.error("Error getting customer data");
-    }
-  }
-};
+//         console.log("subscription charged ");
+//         return epaycoSubscriptionResponse;
+//       } catch (error) {
+//         console.error("Error charging subscription", error);
+//       }
+//     } else {
+//       console.error("Error getting customer data");
+//     }
+//   }
+// };
 
 export async function GET(req: Request, _res: Response) {
-  // if (req.headers.get("Authorization") !== `Bearer ${env.CRON_SECRET}`) {
-  //   return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
-  // }
+  if (req.headers.get("Authorization") !== `Bearer ${env.CRON_SECRET}`) {
+    return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+  }
 
-  // Check if the current date is the starting date of a group
+  // ======================Activate group============================
+
+  // Checks if the current date is the starting date of a group
   const disabledGroups = await db.query.groups.findMany({
     where: eq(groups.currentLevel, 0),
   });
@@ -88,41 +75,21 @@ export async function GET(req: Request, _res: Response) {
   });
 
   if (startingGroup) {
-    const students = await db.query.users.findMany({
-      where: and(
-        eq(users.role, env.STUDENT_ROLE),
-        eq(users.groupId, startingGroup.id),
-      ),
-      columns: {
-        id: true,
-        name: true,
-        planType: true,
-        programId: true,
-      },
-      with: {
-        group: {
-          columns: {
-            startingDate: true,
-          },
-        },
-      },
-    });
-
-    await sequentialForEach(students, async (student) => {
-      db.update(users)
+    try {
+      console.log("Activating students from group: ", startingGroup.name);
+      await db
+        .update(users)
         .set({
           current_level: 1,
         })
-        .where(eq(users.id, student.id))
-        .then(() => {
-          console.log("user activated");
-        })
-        .catch((err) => {
-          console.error(`Error activating user: ${student.id}`, err);
-        });
-    });
+        .where(
+          and(
+            eq(users.role, env.STUDENT_ROLE),
+            eq(users.groupId, startingGroup.id),
+          ),
+        );
 
-    try {
+      console.log("Activating group");
       await db
         .update(groups)
         .set({
@@ -141,57 +108,60 @@ export async function GET(req: Request, _res: Response) {
     where: gt(groups.currentLevel, 0),
   });
 
-  // runningGroups.forEach((group) => {
-  //   const finishLevelDate = getFinishLevelDate(
-  //     new Date(group.startingDate),
-  //     group.currentLevel,
-  //   );
+  runningGroups.forEach((group) => {
+    const finishLevelDate = getFinishLevelDate(
+      new Date(group.startingDate),
+      group.currentLevel,
+    );
 
-  //   const today = new Date().toISOString().slice(0, 10);
-  //   const isTodayEndOfLevel =
-  //     finishLevelDate.toISOString().slice(0, 10) === today;
+    const today = new Date().toISOString().slice(0, 10);
+    const isTodayEndOfLevel =
+      finishLevelDate.toISOString().slice(0, 10) === today;
 
-  //   if (isTodayEndOfLevel) {
-  //     const students = db.query.users.findMany({
-  //       where: and(
-  //         eq(users.role, env.STUDENT_ROLE),
-  //         eq(users.planType, "RECURRENT"),
-  //         eq(users.planType, "LEVEL"),
-  //         eq(users.groupId, group.id),
-  //       ),
-  //       columns: {
-  //         id: true,
-  //         planType: true,
-  //       },
-  //       with: {
-  //         group: {
-  //           columns: {
-  //             startingDate: true,
-  //           },
-  //         },
-  //       },
-  //     });
-  //   }
-  // });
+    if (isTodayEndOfLevel) {
+      db.execute(
+        sql`
+        UPDATE ${users} AS gtu
+        SET
+          active = FALSE
+        FROM
+          (
+            SELECT
+              gtu.id AS student_id,
+              COUNT(gtl.id) AS level_count
+           FROM
+             ${users}
+             LEFT JOIN ${programs} AS gtp ON gtp.id = gtu.program_id
+              LEFT JOIN ${levels} AS gtl ON gtl.program_id = gtp.id
+           GROUP BY
+              gtu.id
+          ) AS sq
+        WHERE
+          gtu.role = ${env.STUDENT_ROLE}
+          AND gtu.group_id = ${group.id}
+          AND sq.level_count < ${group.currentLevel}
+        `,
+      )
+        .then(() => {
+          console.log("Students ended level");
+        })
+        .catch((err) => {
+          console.error(`Error ending level: ${group.id}`, err);
+        });
 
-  // const students = await db.query.users.findMany({
-  //   where: and(
-  //     eq(users.role, env.STUDENT_ROLE),
-  //     eq(users.planType, "RECURRENT"),
-  //     eq(users.planType, "LEVEL"),
-  //   ),
-  //   columns: {
-  //     id: true,
-  //     planType: true,
-  //   },
-  //   with: {
-  //     group: {
-  //       columns: {
-  //         startingDate: true,
-  //       },
-  //     },
-  //   },
-  // });
+      db.update(groups)
+        .set({
+          currentLevel: group.currentLevel + 1,
+        })
+        .where(eq(groups.id, group.id))
+        .then(() => {
+          console.log("Group ended level");
+        })
+        .catch((err) => {
+          console.error(`Error ending level: ${group.id}`, err);
+        });
+    }
+  });
 
   return NextResponse.json({ message: "Request procesada" });
 }
