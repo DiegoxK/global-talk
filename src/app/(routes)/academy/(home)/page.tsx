48a30e34de7da3d8c90ffecfef8b7adb11c060ec
DiegoxK <diego.stv@outlook.es>
2024-10-18 11:21:52 -0500

    Removed unused imports in Home component for cleaner code and improved readability

--- src/app/(routes)/academy/(home)/page.tsx
+++ src/app/(routes)/academy/(home)/page.tsx
@@ -1,6 +1,4 @@
-import { getServerAuthSession } from "@/server/auth";
 import FirstTimeHome from "./_components/first-time-home";
-import { redirect } from "next/navigation";
 import { api } from "@/trpc/server";
 
 export default async function Home() {
