51b306046e3d90c93f8c1f240004f5c22f7b731b
DiegoxK <diego.stv@outlook.es>
2024-10-20 10:54:28 -0500

    Enhance Chat component to display user image and name from session, improving personalization in chat interface

--- src/app/(routes)/academy/chat/page.tsx
+++ src/app/(routes)/academy/chat/page.tsx
@@ -1,12 +1,18 @@
 import { api } from "@/trpc/server";
 import ChatBot from "./chat-bot";
+import { getServerAuthSession } from "@/server/auth";
 
 export default async function Chat() {
   const prompts = await api.prompt.getPrompts();
+  const session = await getServerAuthSession();
 
   return (
     <div className="bg-pattern my-4 h-[calc(100vh-10.5rem)] rounded-sm">
-      <ChatBot prompts={prompts} />
+      <ChatBot
+        prompts={prompts}
+        userImage={session?.user?.image}
+        userName={session?.user?.name}
+      />
     </div>
   );
 }
