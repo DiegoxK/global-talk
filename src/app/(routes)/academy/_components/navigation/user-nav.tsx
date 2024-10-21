4807d6aa7466c0e9ddf023bc2d4143cfc4a529d8
DiegoxK <diego.stv@outlook.es>
2024-10-21 14:27:05 -0500

    Update UserNav to wrap "Mi perfil" button with Link for navigation and add signOut functionality to the logout button

--- src/app/(routes)/academy/_components/navigation/user-nav.tsx
+++ src/app/(routes)/academy/_components/navigation/user-nav.tsx
@@ -1,18 +1,25 @@
+"use client";
+
+import { signOut } from "next-auth/react";
 import { Button } from "@/components/ui/button";
 import { DoorClosed, UserRoundPen } from "lucide-react";
+import Link from "next/link";
 
 export default function UserNav() {
   return (
     <div className="flex flex-col gap-2 pt-2">
-      <Button
-        className="justify-start border-primary text-primary"
-        variant="outline"
-      >
-        <UserRoundPen className="mr-2 h-5 w-5" />
-        Mi perfil
-      </Button>
+      <Link className="w-full" href="/academy/profile">
+        <Button
+          className="w-full justify-start border-primary text-primary"
+          variant="outline"
+        >
+          <UserRoundPen className="mr-2 h-5 w-5" />
+          Mi perfil
+        </Button>
+      </Link>
       <Button
         className="justify-start border-destructive text-destructive hover:bg-red-50 hover:text-destructive"
+        onClick={() => signOut()}
         variant="outline"
       >
         <DoorClosed className="mr-2 h-5 w-5" />
