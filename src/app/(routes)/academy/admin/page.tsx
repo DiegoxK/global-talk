89da39feb2b5303ff0dae2266845ffacfb62414e
DiegoxK <diego.stv@outlook.es>
2024-10-23 07:50:06 -0500

    Add Prompts management link to Admin page for better navigation and user interaction in the academy section

--- src/app/(routes)/academy/admin/page.tsx
+++ src/app/(routes)/academy/admin/page.tsx
@@ -1,4 +1,4 @@
-import { Transactions, Users } from "@/vectors/miscellaneous";
+import { Prompts, Transactions, Users } from "@/vectors/miscellaneous";
 import Link from "next/link";
 
 export default function Admin() {
@@ -9,6 +9,13 @@ export default function Admin() {
           <Users className="mb-6" width={250} height={250} />
           <h2 className="text-2xl font-bold text-primary-600">Usuarios</h2>
           <p>Gestion de usuarios</p>
+        </button>
+      </Link>
+      <Link href="/academy/admin/transactions">
+        <button className="flex h-[410px] w-[410px] flex-col items-center justify-center rounded-md border shadow-md">
+          <Prompts width={280} height={280} />
+          <h2 className="text-2xl font-bold text-primary-600">Prompts</h2>
+          <p>Gestion de prompts</p>
         </button>
       </Link>
       <Link href="/academy/admin/transactions">
