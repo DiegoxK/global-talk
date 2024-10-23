13eb71ba6505ae244ebb6273bae6ceb37bc26553
DiegoxK <diego.stv@outlook.es>
2024-10-23 09:18:57 -0500

    Add Users page component: implement main layout for displaying user prompts in the admin interface with data fetching logic

---
+++ src/app/(routes)/academy/admin/prompts/page.tsx
@@ -0,0 +1,18 @@
+import { Separator } from "@/components/ui/separator";
+import { columns } from "./components/columns";
+import { DataTable } from "./components/data-table";
+import { api } from "@/trpc/server";
+
+export default async function Users() {
+  const prompts = await api.prompt.getAllPrompts();
+
+  return (
+    <div className="mt-4">
+      <h1 className="text-xl font-bold text-primary">Gestion de usuarios</h1>
+      <Separator />
+      <div className="mt-4">
+        <DataTable columns={columns} data={prompts} />
+      </div>
+    </div>
+  );
+}
