a6702386b83c37c66cbffd800d2338bc1d3bcf3b
DiegoxK <diego.stv@outlook.es>
2024-10-23 09:17:04 -0500

    Implement prompt management for admins: add create, update, delete, and fetch prompts with role-based access control

--- src/server/api/routers/prompt.ts
+++ src/server/api/routers/prompt.ts
@@ -4,6 +4,7 @@ import OpenAI from "openai";
 import { and, count, eq } from "drizzle-orm";
 import { z } from "zod";
 import { ChatCompletionRole } from "openai/resources/index.mjs";
+import { env } from "@/env";
 
 const openai = new OpenAI();
 
@@ -23,6 +24,82 @@ export const promptRouter = createTRPCRouter({
       })
       .from(prompts);
   }),
+
+  getAllPrompts: protectedProcedure.query(async ({ ctx }) => {
+    const user = ctx.session.user;
+
+    if (user.role !== env.ADMIN_ROLE) {
+      throw new Error("Unauthorized");
+    }
+
+    return ctx.db.query.prompts.findMany();
+  }),
+
+  createPrompt: protectedProcedure
+    .input(
+      z.object({
+        name: z.string(),
+        prompt: z.string(),
+        description: z.string(),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      const user = ctx.session.user;
+
+      if (user.role !== env.ADMIN_ROLE) {
+        throw new Error("Unauthorized");
+      }
+
+      return await ctx.db.insert(prompts).values({
+        name: input.name,
+        prompt: input.prompt,
+        description: input.description,
+      });
+    }),
+
+  updatePrompt: protectedProcedure
+    .input(
+      z.object({
+        id: z.string().uuid().optional(),
+        name: z.string(),
+        prompt: z.string(),
+        description: z.string(),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      const user = ctx.session.user;
+
+      if (user.role !== env.ADMIN_ROLE) {
+        throw new Error("Unauthorized");
+      }
+
+      if (input.id) {
+        return await ctx.db
+          .update(prompts)
+          .set({
+            name: input.name,
+            prompt: input.prompt,
+            description: input.description,
+          })
+          .where(eq(prompts.id, input.id));
+      }
+    }),
+
+  deletePrompt: protectedProcedure
+    .input(
+      z.object({
+        id: z.string().uuid(),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      const user = ctx.session.user;
+
+      if (user.role !== env.ADMIN_ROLE) {
+        throw new Error("Unauthorized");
+      }
+
+      return await ctx.db.delete(prompts).where(eq(prompts.id, input.id));
+    }),
 
   getAiResponse: protectedProcedure
     .input(
