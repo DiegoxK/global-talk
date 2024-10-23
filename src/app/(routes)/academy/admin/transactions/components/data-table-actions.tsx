fe319d4c02912ca4487b3b2ea663b52cf6aedc65
DiegoxK <diego.stv@outlook.es>
2024-10-23 06:51:33 -0500

    Add DataTableActions component for transaction management, enabling editing and deletion actions in the dropdown menu

---
+++ src/app/(routes)/academy/admin/transactions/components/data-table-actions.tsx
@@ -0,0 +1,54 @@
+import type { Transaction } from "@/lib/definitions";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { MoreHorizontal } from "lucide-react";
+import type { Dispatch, SetStateAction } from "react";
+
+interface DataTableActionsProps {
+  transaction: Transaction;
+  setTransaction: Dispatch<SetStateAction<Transaction | undefined | null>>;
+  setIsDialogOpen: Dispatch<SetStateAction<boolean>>;
+}
+
+export default function DataTableActions({
+  transaction,
+  setTransaction,
+  setIsDialogOpen,
+}: DataTableActionsProps) {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" className="h-8 w-8 p-0">
+          <span className="sr-only">Abrir menu</span>
+          <MoreHorizontal className="h-4 w-4" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuLabel>Acciones</DropdownMenuLabel>
+        <DropdownMenuItem
+          onClick={() => {
+            setTransaction(transaction);
+            setIsDialogOpen(true);
+          }}
+        >
+          Editar
+        </DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem
+          className="text-destructive focus:bg-red-100 focus:text-destructive"
+          onClick={() => navigator.clipboard.writeText(transaction.description)}
+        >
+          Eliminar
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
