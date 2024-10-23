802c9a04918996f7a9e96177d4304570650fc679
DiegoxK <diego.stv@outlook.es>
2024-10-23 07:51:02 -0500

    Remove onSubmit function and form fields from DataTableDialog component for simplified view without user input capabilities

--- src/app/(routes)/academy/admin/transactions/components/data-table-dialog.tsx
+++ src/app/(routes)/academy/admin/transactions/components/data-table-dialog.tsx
@@ -84,10 +84,6 @@ export default function DataTableDialog({
     defaultValues: {},
   });
 
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    console.log(values);
-  }
-
   return (
     <Dialog
       open={open}
@@ -102,124 +98,7 @@ export default function DataTableDialog({
         setOpen(open);
       }}
     >
-      <DialogContent className="max-h-[90vh]">
-        <DialogHeader>
-          <DialogTitle>
-            {isEditing ? "Editar usuario" : "Crear usuario"}
-          </DialogTitle>
-          <DialogDescription>
-            {isEditing
-              ? "Formulario de edicion del usuario."
-              : "Formulario de creacion del usuario."}
-          </DialogDescription>
-        </DialogHeader>
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)}>
-            <div className="max-h-[60vh] space-y-2 overflow-y-auto rounded-sm px-2 pb-2">
-              <FormField
-                control={form.control}
-                name="name"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>
-                      Nombre <Required /> <FormMessage />
-                    </FormLabel>
-                    <FormControl>
-                      <Input placeholder="Antonio" {...field} />
-                    </FormControl>
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="lastName"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>
-                      Apellido <Required /> <FormMessage />
-                    </FormLabel>
-                    <FormControl>
-                      <Input placeholder="Casas" {...field} />
-                    </FormControl>
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="email"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>
-                      Correo electronico <Required /> <FormMessage />
-                    </FormLabel>
-                    <FormControl>
-                      <Input
-                        type="email"
-                        placeholder="ejemplo@globtm.co"
-                        {...field}
-                      />
-                    </FormControl>
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="programId"
-                render={({ field }) => (
-                  <FormItem className="flex flex-col">
-                    <FormLabel>
-                      Curso <Required /> <FormMessage />
-                    </FormLabel>
-                    <Combobox
-                      fieldName="curso"
-                      values={programs}
-                      field={field}
-                    />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="role"
-                render={({ field }) => (
-                  <FormItem className="flex flex-col">
-                    <FormLabel>
-                      Rol <Required /> <FormMessage />
-                    </FormLabel>
-                    <Combobox
-                      fieldName="Rol"
-                      values={[
-                        {
-                          label: "Admin",
-                          value: "Admin",
-                        },
-                        {
-                          label: "Estudiante",
-                          value: "Estudiante",
-                        },
-                        {
-                          label: "Profesor",
-                          value: "Profesor",
-                        },
-                      ]}
-                      field={field}
-                    />
-                  </FormItem>
-                )}
-              />
-            </div>
-            <DialogFooter className="mt-2">
-              <Button
-                disabled={!form.formState.isDirty}
-                className="rounded-sm"
-                type="submit"
-              >
-                {isEditing ? "Actualizar usuario" : "Crear usuario"}
-              </Button>
-            </DialogFooter>
-          </form>
-        </Form>
-      </DialogContent>
+      <DialogContent className="max-h-[90vh]"></DialogContent>
     </Dialog>
   );
 }
