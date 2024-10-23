5fe48ddcab6f183ecbaf40c50f6cffe73e7d070b
DiegoxK <diego.stv@outlook.es>
2024-10-23 07:50:23 -0500

    Integrate transactions display in the admin panel by fetching data and rendering it in a DataTable for improved management

--- src/app/(routes)/academy/admin/transactions/page.tsx
+++ src/app/(routes)/academy/admin/transactions/page.tsx
@@ -1,12 +1,20 @@
 import { Separator } from "@/components/ui/separator";
+import { columns } from "./components/columns";
+import { DataTable } from "./components/data-table";
+import { api } from "@/trpc/server";
 
-export default function Transactions() {
+export default async function Users() {
+  const transactions = await api.transaction.getAllTransactions();
+
   return (
     <div className="mt-4">
       <h1 className="text-xl font-bold text-primary">
         Gestion de transacciones
       </h1>
       <Separator />
+      <div className="mt-4">
+        <DataTable columns={columns} data={transactions} />
+      </div>
     </div>
   );
 }
