eef0c0d10af3dba1c499e1e10b805b58ead692c2
DiegoxK <diego.stv@outlook.es>
2024-10-23 08:46:46 -0500

    Implement alert dialog for user deletion confirmation in DataTableActions, enhancing safety before user removal

--- src/app/(routes)/academy/admin/users/components/data-table-actions.tsx
+++ src/app/(routes)/academy/admin/users/components/data-table-actions.tsx
@@ -1,5 +1,16 @@
 import type { UserWithRole } from "@/lib/definitions";
 
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "@/components/ui/alert-dialog";
+
 import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
@@ -10,7 +21,9 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { MoreHorizontal } from "lucide-react";
-import type { Dispatch, SetStateAction } from "react";
+import { useState, type Dispatch, type SetStateAction } from "react";
+import { api } from "@/trpc/react";
+import { useRouter } from "next/navigation";
 
 interface DataTableActionsProps {
   user: UserWithRole;
@@ -23,32 +36,68 @@ export default function DataTableActions({
   setUser,
   setIsDialogOpen,
 }: DataTableActionsProps) {
+  const router = useRouter();
+
+  const [openAlert, setOpenAlert] = useState(false);
+  const { mutate: deleteUser } = api.user.deleteUser.useMutation({
+    onSuccess: () => {
+      router.refresh();
+    },
+  });
+
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="ghost" className="h-8 w-8 p-0">
-          <span className="sr-only">Abrir menu</span>
-          <MoreHorizontal className="h-4 w-4" />
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuLabel>Acciones</DropdownMenuLabel>
-        <DropdownMenuItem
-          onClick={() => {
-            setUser(user);
-            setIsDialogOpen(true);
-          }}
-        >
-          Editar
-        </DropdownMenuItem>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem
-          className="text-destructive focus:bg-red-100 focus:text-destructive"
-          onClick={() => navigator.clipboard.writeText(user.name)}
-        >
-          Eliminar
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <>
+      <AlertDialog open={openAlert} onOpenChange={setOpenAlert}>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Estas seguro?</AlertDialogTitle>
+            <AlertDialogDescription>
+              Esta acción no se puede deshacer. Esto eliminará el usuario y no
+              se podrá recuperar.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancelar</AlertDialogCancel>
+            <AlertDialogAction
+              onClick={() => {
+                deleteUser({ userEmail: user.email });
+                setOpenAlert(false);
+              }}
+              className="rounded-sm bg-destructive text-destructive-foreground hover:bg-background hover:text-destructive hover:outline hover:outline-1 hover:outline-destructive"
+            >
+              Continuar
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="ghost" className="h-8 w-8 p-0">
+            <span className="sr-only">Abrir menu</span>
+            <MoreHorizontal className="h-4 w-4" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuLabel>Acciones</DropdownMenuLabel>
+          <DropdownMenuItem
+            onClick={() => {
+              setUser(user);
+              setIsDialogOpen(true);
+            }}
+          >
+            Editar
+          </DropdownMenuItem>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem
+            className="text-destructive focus:bg-red-100 focus:text-destructive"
+            onClick={() => {
+              setOpenAlert(true);
+            }}
+          >
+            Eliminar
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </>
   );
 }
