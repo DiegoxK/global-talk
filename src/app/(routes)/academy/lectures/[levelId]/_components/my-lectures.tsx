5d6d7590e080f336da4eaf3f27a6bae891c4d998
DiegoxK <diego.stv@outlook.es>
2024-10-24 09:06:29 -0500

    Enhance MyLectures component to display empty states for scheduled, available, and finished lectures when no data is present

--- src/app/(routes)/academy/lectures/[levelId]/_components/my-lectures.tsx
+++ src/app/(routes)/academy/lectures/[levelId]/_components/my-lectures.tsx
@@ -36,48 +36,66 @@ export default function MyLectures({
         <h1 className="text-xl font-bold text-primary">Clases agendadas</h1>
         <Separator />
         <div className="flex flex-wrap gap-2 py-2">
-          {scheduledLectures.map((lecture) => {
-            return (
-              <LectureCard
-                action={() => {
-                  setOpen(true);
-                  setState("view");
-                  setLecture(lecture);
-                }}
-                state="scheduled"
-                key={lecture.id}
-                lecture={lecture}
-                scheduleCount={lecture.schedulesCount}
-              />
-            );
-          })}
+          {scheduledLectures.length > 0 ? (
+            scheduledLectures.map((lecture) => {
+              return (
+                <LectureCard
+                  action={() => {
+                    setOpen(true);
+                    setState("view");
+                    setLecture(lecture);
+                  }}
+                  state="scheduled"
+                  key={lecture.id}
+                  lecture={lecture}
+                  scheduleCount={lecture.schedulesCount}
+                />
+              );
+            })
+          ) : (
+            <div className="mt-5 flex h-[100px] w-full items-center justify-center text-muted-foreground">
+              Aún no has agendado ninguna clase.
+            </div>
+          )}
         </div>
       </div>
       <div>
         <h1 className="text-xl font-bold text-primary">Clases disponibles</h1>
         <Separator />
-        <SortedLectures
-          action={() => {
-            setOpen(true);
-            setState("schedule");
-          }}
-          state={"available"}
-          setLecture={setLecture}
-          lectureSessions={availableLectures}
-        />
+        {availableLectures.length > 0 ? (
+          <SortedLectures
+            action={() => {
+              setOpen(true);
+              setState("schedule");
+            }}
+            state={"available"}
+            setLecture={setLecture}
+            lectureSessions={availableLectures}
+          />
+        ) : (
+          <div className="mt-5 flex h-[100px] w-full items-center justify-center text-muted-foreground">
+            Te encuentras al día!
+          </div>
+        )}
       </div>
-      <div>
+      <div className="mb-4">
         <h1 className="text-xl font-bold text-primary">Clases finalizadas</h1>
         <Separator />
-        <SortedLectures
-          action={() => {
-            setOpen(true);
-            setState("view");
-          }}
-          state={"finished"}
-          setLecture={setLecture}
-          lectureSessions={finishedLectures}
-        />
+        {finishedLectures.length > 0 ? (
+          <SortedLectures
+            action={() => {
+              setOpen(true);
+              setState("view");
+            }}
+            state={"finished"}
+            setLecture={setLecture}
+            lectureSessions={finishedLectures}
+          />
+        ) : (
+          <div className="mt-5 flex h-[100px] w-full items-center justify-center text-muted-foreground">
+            Aún no se han finalizado clases en este nivel.
+          </div>
+        )}
       </div>
     </>
   );
