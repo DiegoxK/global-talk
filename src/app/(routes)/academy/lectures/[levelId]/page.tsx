d26fc8221f9e06db9805c6ac9ddc76e61a8f75a0
DiegoxK <diego.stv@outlook.es>
2024-10-24 08:35:14 -0500

    Update Lectures page to fetch and display finished lectures alongside available and scheduled lectures

--- src/app/(routes)/academy/lectures/[levelId]/page.tsx
+++ src/app/(routes)/academy/lectures/[levelId]/page.tsx
@@ -6,16 +6,19 @@ interface LecturesProps {
 }
 
 export default async function Lectures({ params: { levelId } }: LecturesProps) {
-  const [availableLectures, scheduledLectures] = await Promise.all([
-    api.lectureSession.getAvailableLectureSessions({ levelId }),
-    api.lectureSession.getScheduledLectureSessions({ levelId }),
-  ]);
+  const [availableLectures, scheduledLectures, finishedLectures] =
+    await Promise.all([
+      api.lectureSession.getAvailableLectureSessions({ levelId }),
+      api.lectureSession.getScheduledLectureSessions({ levelId }),
+      api.lectureSession.getFinishedLectureSessions({ levelId }),
+    ]);
 
   return (
     <div className="flex w-full flex-col">
       <MyLectures
         availableLectures={availableLectures}
         scheduledLectures={scheduledLectures}
+        finishedLectures={finishedLectures}
       />
     </div>
   );
