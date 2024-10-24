54453520ed5d7b741b709ff2ad890bbaca3daa71
DiegoxK <diego.stv@outlook.es>
2024-10-24 11:22:19 -0500

    Implement new LecturesLayout with user-level data and right sidebar, removing the old layout for better structure

---
+++ src/app/(routes)/academy/lectures/[levelId]/layout.tsx
@@ -0,0 +1,23 @@
+import { api } from "@/trpc/server";
+import RightSidebar from "../../_components/ui/right-sidebar";
+
+export default async function LecturesLayout({
+  children,
+  params,
+}: {
+  children: React.ReactNode;
+  params: { levelId: string };
+}) {
+  const userInformation = await api.level.getUserLevels();
+
+  return (
+    <div className="flex justify-between">
+      {children}
+      <RightSidebar
+        levelId={params.levelId}
+        levels={userInformation.levels}
+        currentUserLevel={userInformation.currentUserLevel}
+      />
+    </div>
+  );
+}
