95fd4df437b22b8a84f00f745aec9bfb33be731f
DiegoxK <diego.stv@outlook.es>
2024-10-24 11:22:30 -0500

    Add disabled state to LevelLink component, rendering as a div when disabled for improved usability and styling consistency

--- src/app/(routes)/academy/_components/navigation/level-link.tsx
+++ src/app/(routes)/academy/_components/navigation/level-link.tsx
@@ -6,12 +6,18 @@ import { Sparkles } from "lucide-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
-export default function LevelLink({ level }: { level: Level }) {
+export default function LevelLink({
+  level,
+  disabled,
+}: {
+  level: Level;
+  disabled?: boolean;
+}) {
   const path = usePathname();
-  return (
+  return disabled ? (
     <Link
       className={cn(
-        "flex w-full items-center gap-2 rounded-md bg-primary-400 px-4 py-2 text-white",
+        "flex w-full items-center gap-2 rounded-md bg-primary px-4 py-2 text-white",
         path.includes(level.id) && "bg-primary-700",
       )}
       href={`/academy/lectures/${level.id}`}
@@ -19,5 +25,14 @@ export default function LevelLink({ level }: { level: Level }) {
       <Sparkles className="h-5 w-5" />
       {level.name}
     </Link>
+  ) : (
+    <div
+      className={cn(
+        "flex w-full select-none items-center gap-2 rounded-md bg-primary-200 px-4 py-2 text-white",
+      )}
+    >
+      <Sparkles className="h-5 w-5" />
+      {level.name}
+    </div>
   );
 }
