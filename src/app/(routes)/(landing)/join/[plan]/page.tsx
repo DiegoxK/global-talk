33d5321de98342be75705f6f0e66cca600f80fd6
DiegoxK <diego.stv@outlook.es>
2024-10-28 09:28:32 -0500

    Add Pure A1 plan component to join page, enhancing offering with detailed plan options and improved UI elements

--- src/app/(routes)/(landing)/join/[plan]/page.tsx
+++ src/app/(routes)/(landing)/join/[plan]/page.tsx
@@ -9,6 +9,7 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import Link from "next/link";
+import { cn } from "@/lib/utils";
 
 export default function Page({ params }: { params: { plan: string } }) {
   const planId = params.plan;
@@ -16,10 +17,92 @@ export default function Page({ params }: { params: { plan: string } }) {
   switch (planId) {
     case "beginners_a0":
       return <BeginnersA0 planId={planId} />;
+    case "pure_a1":
+      return <PureA1 planId={planId} />;
   }
 }
 
 const BeginnersA0 = ({ planId }: { planId: string }) => {
+  const plans = [
+    {
+      href: `/checkout/${planId}/one-time-level`,
+      title: "Pago único por nivel",
+      description: "Matrícula individual",
+      price: "530 mil COP / nivel",
+      duration: "2 meses",
+      features: [
+        "32 clases de una hora (4 por semana)",
+        "Acceso a la plataforma de aprendizaje",
+        "Grabaciones de las clases",
+        "Todos los materiales",
+        "Evaluación al final del nivel",
+        "1 cupo en un grupo de máximo 5 personas",
+      ],
+    },
+
+    {
+      href: `/checkout/${planId}/one-time`,
+      title: "Programa completo",
+      description: "Matrícula individual - Programa completo",
+      price: "1 millón COP / programa",
+      duration: "4 meses (2 niveles)",
+      features: [
+        "64 clases de una hora (4 por semana)",
+        "Acceso a la plataforma de aprendizaje",
+        "Grabaciones de las clases",
+        "Todos los materiales",
+        "Evaluación al final de cada nivel",
+        "1 cupo en un grupo de máximo 5 personas",
+      ],
+      savings: "Ahorras 200 mil pesos",
+    },
+  ];
+
+  return (
+    <div className="container mx-auto flex flex-col items-center justify-center gap-12 border-t py-16">
+      <h1 className="text-4xl font-bold text-primary">
+        Beginners A0 - Matrícula individual
+      </h1>
+      <div className="flex flex-wrap last:gap-6">
+        {plans.map((plan, index) => (
+          <div key={index}>
+            <Card
+              className={cn("relative flex flex-col", plan.savings && "pt-16")}
+            >
+              {plan.savings && <div className="ribbon">{plan.savings}</div>}
+              <CardHeader>
+                <CardTitle>{plan.title}</CardTitle>
+                <CardDescription>{plan.description}</CardDescription>
+              </CardHeader>
+              <CardContent className="mb-4 flex-grow">
+                <p className="mb-2 text-2xl font-bold text-primary">
+                  {plan.price}
+                </p>
+                <p className="mb-4 text-sm text-muted-foreground">
+                  Duración: {plan.duration}
+                </p>
+                <ul className="space-y-2">
+                  {plan.features.map((feature, featureIndex) => (
+                    <li key={featureIndex} className="flex items-start">
+                      <Check className="mr-2 mt-0.5 h-5 w-5 text-green-500" />
+                      <span className="text-sm">{feature}</span>
+                    </li>
+                  ))}
+                </ul>
+              </CardContent>
+
+              <Link href={plan.href}>
+                <Button className="mt-1 w-full">Seleccionar Plan</Button>
+              </Link>
+            </Card>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const PureA1 = ({ planId }: { planId: string }) => {
   const plans = [
     {
       href: `/checkout/${planId}/one-time-level`,
