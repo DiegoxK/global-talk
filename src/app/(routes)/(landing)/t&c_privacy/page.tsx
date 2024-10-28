a9fa923b78ae935a4c195233f5f6498c4724e7df
DiegoxK <diego.stv@outlook.es>
2024-10-28 10:45:13 -0500

    Add comprehensive Privacy Policy content, detailing data collection, usage, and user rights to enhance transparency and compliance

--- src/app/(routes)/(landing)/t&c_privacy/page.tsx
+++ src/app/(routes)/(landing)/t&c_privacy/page.tsx
@@ -1,3 +1,237 @@
 export default function Privacy() {
-  return <div>Privacy</div>;
+  return (
+    <div className="space-y-4 border-t px-64 py-12">
+      <h1 className="text-4xl font-bold">Política de Privacidad</h1>
+      <h2 className="text-2xl font-bold">1. Introducción</h2>
+      <p>
+        Bienvenido a Global Talk Medallo. Esta Política de Privacidad explica
+        cómo recopilamos, usamos, divulgamos y protegemos la información
+        personal que recopilamos de los usuarios a través de nuestra plataforma
+        en línea globtm.co y cualquier otro servicio proporcionado por nosotros.
+      </p>
+      <p>
+        Al acceder y utilizar nuestros servicios, usted acepta las prácticas
+        descritas en esta Política de Privacidad. Si no está de acuerdo con esta
+        política, por favor no utilice nuestros servicios.
+      </p>
+
+      <h2 className="text-2xl font-bold">2. Información que Recopilamos</h2>
+      <h3 className="text-xl font-bold">2.1. Información Personal</h3>
+      <ul className="ms-5 list-disc space-y-2">
+        <li>
+          <strong>Información de Registro:</strong> Cuando se registra en
+          nuestra plataforma, recopilamos información personal como su nombre
+          completo, dirección de correo electrónico, número de teléfono y
+          cualquier otra información necesaria para crear y mantener su cuenta.
+        </li>
+        <li>
+          <strong>Información de Pago:</strong> La información de pago no es
+          almacenada por Global Talk Medallo, esta información es manejada y
+          procesada por un proveedor de servicios de pago externo conforme a su
+          propia política de privacidad.
+        </li>
+        <li>
+          <strong>Información de Curso:</strong> Recopilamos información sobre
+          su participación en nuestros cursos, incluidos los cursos que ha
+          tomado, su progreso, las calificaciones obtenidas, y cualquier otra
+          interacción que tenga con el contenido de los cursos.
+        </li>
+      </ul>
+
+      <h3 className="text-xl font-bold">2.2. Información No Personal</h3>
+      <ul className="ms-5 list-disc space-y-2">
+        <li>
+          <strong>Datos de Uso:</strong> Recopilamos información sobre su
+          interacción con nuestra plataforma, como su tipo de navegador, sistema
+          operativo, páginas visitadas, tiempo de permanencia en la plataforma,
+          y otros datos técnicos relacionados con su acceso y uso de los
+          servicios.
+        </li>
+        <li>
+          <strong>Cookies y Tecnologías Similares:</strong> Utilizamos cookies y
+          tecnologías similares para mejorar su experiencia en nuestra
+          plataforma, facilitar el inicio de sesión, recordar sus preferencias,
+          y analizar el tráfico del sitio. Puede configurar su navegador para
+          rechazar cookies, pero esto puede afectar la funcionalidad de la
+          plataforma.
+        </li>
+      </ul>
+
+      <h2 className="text-2xl font-bold">3. Cómo Utilizamos su Información</h2>
+      <h3 className="text-xl font-bold">3.1. Provisión de Servicios</h3>
+      <ul className="ms-5 list-disc space-y-2">
+        <li>
+          <strong>Gestión de Cuenta:</strong> Utilizamos su información personal
+          para crear y administrar su cuenta, ofrecerle acceso a los cursos,
+          proporcionar soporte técnico, y comunicarnos con usted sobre cualquier
+          asunto relacionado con su cuenta o el uso de nuestros servicios.
+        </li>
+      </ul>
+
+      <h3 className="text-xl font-bold">3.2. Mejora de Servicios</h3>
+      <ul className="ms-5 list-disc space-y-2">
+        <li>
+          <strong>Personalización:</strong> Utilizamos su información para
+          personalizar su experiencia en la plataforma, como recomendarle cursos
+          relevantes o ajustar el contenido de acuerdo a sus preferencias.
+        </li>
+        <li>
+          <strong>Análisis y Estadísticas:</strong> Analizamos la información de
+          uso y datos agregados para mejorar el diseño, funcionalidad y
+          contenido de nuestra plataforma, así como para realizar
+          investigaciones y análisis de mercado.
+        </li>
+      </ul>
+
+      <h3 className="text-xl font-bold">3.3. Marketing y Comunicaciones</h3>
+      <ul className="ms-5 list-disc space-y-2">
+        <li>
+          <strong>Boletines y Promociones:</strong> Con su consentimiento,
+          utilizamos su dirección de correo electrónico y otra información de
+          contacto para enviarle boletines, promociones, y otras comunicaciones
+          relacionadas con los servicios de la Academia. Puede optar por no
+          recibir estas comunicaciones en cualquier momento.
+        </li>
+        <li>
+          <strong>Publicidad Dirigida:</strong> Utilizamos su información para
+          mostrarle anuncios relevantes en nuestra plataforma o en sitios web de
+          terceros, basados en sus intereses y comportamiento en línea.
+        </li>
+      </ul>
+
+      <h2 className="text-2xl font-bold">
+        4. Compartir y Divulgar su Información
+      </h2>
+      <h3 className="text-xl font-bold">4.1. Proveedores de Servicios</h3>
+      <ul className="ms-5 list-disc space-y-2">
+        <li>
+          <strong>Proveedores Externos:</strong> Compartimos su información
+          personal con proveedores externos que nos asisten en la operación de
+          la plataforma, como procesadores de pagos, proveedores de hosting,
+          servicios de correo electrónico, y analítica. Estos proveedores están
+          obligados a proteger su información y usarla solo para los fines para
+          los que fueron contratados.
+        </li>
+      </ul>
+
+      <h3 className="text-xl font-bold">4.2. Cumplimiento Legal</h3>
+      <ul className="ms-5 list-disc space-y-2">
+        <li>
+          <strong>Obligaciones Legales:</strong> Podemos divulgar su información
+          personal si así lo requiere la ley, una orden judicial, o cualquier
+          otra autoridad gubernamental para cumplir con una obligación legal o
+          proteger nuestros derechos, propiedad o seguridad, así como la de
+          nuestros usuarios u otros terceros.
+        </li>
+      </ul>
+
+      <h3 className="text-xl font-bold">4.3. Transacciones Corporativas</h3>
+      <ul className="ms-5 list-disc space-y-2">
+        <li>
+          <strong>Fusiones y Adquisiciones:</strong> En caso de que la Academia
+          sea objeto de una fusión, adquisición, reestructuración, o venta de
+          activos, su información personal podría ser transferida como parte de
+          la transacción. Le notificaremos cualquier cambio en la propiedad o
+          uso de su información antes de que se realice la transferencia.
+        </li>
+      </ul>
+
+      <h2 className="text-2xl font-bold">5. Seguridad de la Información</h2>
+      <ul className="ms-5 list-disc space-y-2">
+        <li>
+          <strong>Medidas de Seguridad:</strong> Implementamos medidas técnicas
+          y organizativas razonables para proteger su información personal
+          contra el acceso no autorizado, pérdida, destrucción o alteración.
+          Esto incluye el uso de cifrado, firewalls, y control de acceso. Sin
+          embargo, ningún sistema es completamente seguro, y no podemos
+          garantizar la seguridad absoluta de su información.
+        </li>
+        <li>
+          <strong>Conservación de Datos:</strong> Conservamos su información
+          personal solo durante el tiempo necesario para cumplir con los fines
+          para los que fue recopilada o según lo exija la ley aplicable. Una vez
+          que su información ya no sea necesaria, la eliminaremos de manera
+          segura.
+        </li>
+      </ul>
+      <h2 className="text-2xl font-bold">6. Sus Derechos</h2>
+      <ul className="ms-5 list-disc space-y-2">
+        <li>
+          <strong>Acceso y Rectificación:</strong> Usted tiene derecho a acceder
+          y solicitar la rectificación de su información personal almacenada por
+          nosotros. Si desea revisar, actualizar o corregir su información,
+          puede hacerlo a través de su cuenta en la plataforma o contactándonos
+          directamente.
+        </li>
+        <li>
+          <strong>Cancelación y Oposición:</strong> Puede solicitar la
+          cancelación de su cuenta o oponerse al tratamiento de su información
+          personal en cualquier momento. Si desea ejercer este derecho,
+          contáctenos utilizando la información de contacto proporcionada en
+          esta política.
+        </li>
+        <li>
+          <strong>Portabilidad de Datos:</strong> Usted tiene derecho a
+          solicitar una copia de su información personal en un formato
+          estructurado, de uso común y lectura mecánica. Esto le permite
+          trasladar su información a otro proveedor de servicios si así lo
+          desea.
+        </li>
+      </ul>
+      <h2 className="text-2xl font-bold">7. Privacidad de los Menores</h2>
+      <ul className="ms-5 list-disc space-y-2">
+        <li>
+          <strong>Uso por Menores:</strong> No recopilamos ni solicitamos
+          intencionadamente información personal de menores de 18 años sin el
+          consentimiento verificable de sus padres o tutores legales. Si
+          descubrimos que hemos recopilado información personal de un menor sin
+          el consentimiento adecuado, eliminaremos dicha información lo antes
+          posible.
+        </li>
+        <li>
+          <strong>Supervisión Parental:</strong> Recomendamos a los padres y
+          tutores legales que supervisen las actividades en línea de sus hijos y
+          que utilicen herramientas de control parental para proteger a los
+          menores.
+        </li>
+      </ul>
+
+      <h2 className="text-2xl font-bold">
+        8. Cambios a esta Política de Privacidad
+      </h2>
+      <ul className="ms-5 list-disc space-y-2">
+        <li>
+          <strong>Actualizaciones:</strong> Nos reservamos el derecho de
+          actualizar esta Política de Privacidad en cualquier momento. Cualquier
+          cambio significativo en la forma en que tratamos su información
+          personal será notificado a través de la plataforma o mediante un aviso
+          por correo electrónico antes de que entren en vigor.
+        </li>
+        <li>
+          <strong>Revisión Continua:</strong> Le recomendamos que revise esta
+          Política de Privacidad periódicamente para estar al tanto de cualquier
+          cambio. El uso continuado de nuestros servicios después de la
+          publicación de cambios en esta política se considerará como aceptación
+          de los mismos.
+        </li>
+      </ul>
+
+      <h2 className="text-2xl font-bold">9. Contacto</h2>
+      <p>
+        <strong>Información de Contacto:</strong> Si tiene alguna pregunta,
+        comentario o solicitud relacionada con esta Política de Privacidad o
+        sobre cómo manejamos su información personal, no dude en contactarnos a
+        través de:
+      </p>
+      <ul className="ms-5 list-disc space-y-2">
+        {/* TODO: Change to actual domain and number */}
+        <li>
+          <strong>Correo Electrónico:</strong> soporte@globtm.co
+        </li>
+        <li>
+          <strong>Teléfono:</strong> 300 000 0000
+        </li>
+      </ul>
+    </div>
+  );
 }
