51587915c1d2572b1b4a31b16564306e58e82146
DiegoxK <diego.stv@outlook.es>
2024-10-29 14:33:18 -0500

    Add new pricing plans and details for various courses on the join page to enhance user experience and selection process

--- src/app/(routes)/(landing)/join/[plan]/page.tsx
+++ src/app/(routes)/(landing)/join/[plan]/page.tsx
@@ -17,8 +17,18 @@ export default function Page({ params }: { params: { plan: string } }) {
   switch (planId) {
     case "beginners_a0":
       return <BeginnersA0 planId={planId} />;
-    // case "pure_a1":
-    //   return <PureA1 planId={planId} />;
+    case "pure_a1":
+      return <PureA1 planId={planId} />;
+    case "beginners_plus_pure_a1":
+      return <BeginnersPlusPureA1 planId={planId} />;
+    case "from_a2_to_b1":
+      return <FromA2ToB1 planId={planId} />;
+    case "from_b1_to_b2":
+      return <FromB1ToB2 planId={planId} />;
+    case "intensive_b2":
+      return <IntensiveB2 planId={planId} />;
+    case "business_english":
+      return <BusinessEnglish planId={planId} />;
   }
 }
 
@@ -62,6 +72,468 @@ const BeginnersA0 = ({ planId }: { planId: string }) => {
     <div className="container mx-auto flex flex-col items-center justify-center gap-12 border-t py-16">
       <h1 className="text-4xl font-bold text-primary">
         Beginners A0 - Matrícula individual
+      </h1>
+      <div className="flex flex-wrap last:gap-6">
+        {plans.map((plan, index) => (
+          <div key={index}>
+            <Card className="relative flex flex-col">
+              {plan.savings && <div className="ribbon">{plan.savings}</div>}
+              <CardHeader>
+                <CardTitle>{plan.title}</CardTitle>
+                <CardDescription>{plan.description}</CardDescription>
+              </CardHeader>
+              <CardContent className="mb-4 flex-grow">
+                <p className="mb-2 text-2xl font-bold text-primary">
+                  {plan.price}
+                </p>
+                <p className="mb-4 text-sm text-muted-foreground">
+                  Duración: {plan.duration}
+                </p>
+                <ul className="space-y-2">
+                  {plan.features.map((feature, featureIndex) => (
+                    <li key={featureIndex} className="flex items-start">
+                      <Check className="mr-2 mt-0.5 h-5 w-5 text-green-500" />
+                      <span className="text-sm">{feature}</span>
+                    </li>
+                  ))}
+                </ul>
+              </CardContent>
+
+              <Link href={plan.href}>
+                <Button className="mt-1 w-full">Seleccionar Plan</Button>
+              </Link>
+            </Card>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+const PureA1 = ({ planId }: { planId: string }) => {
+  const plans = [
+    {
+      href: `/checkout/${planId}/one-time-level`,
+      title: "Pago único por nivel",
+      description: "Matrícula individual",
+      price: "530 mil COP / nivel",
+      duration: "2 meses",
+      features: [
+        "32 clases de una hora (4 por semana)",
+        "Acceso a la plataforma de aprendizaje",
+        "Grabaciones de las clases",
+        "Todos los materiales",
+        "Evaluación al final del nivel",
+        "1 cupo en un grupo de máximo 5 personas",
+      ],
+    },
+
+    {
+      href: `/checkout/${planId}/one-time`,
+      title: "Programa completo",
+      description: "Matrícula individual - Programa completo",
+      price: "1 millón COP / programa",
+      duration: "4 meses (2 niveles)",
+      features: [
+        "64 clases de una hora (4 por semana)",
+        "Acceso a la plataforma de aprendizaje",
+        "Grabaciones de las clases",
+        "Todos los materiales",
+        "Evaluación al final de cada nivel",
+        "1 cupo en un grupo de máximo 5 personas",
+      ],
+      savings: "Ahorras 200 mil pesos",
+    },
+  ];
+
+  return (
+    <div className="container mx-auto flex flex-col items-center justify-center gap-12 border-t py-16">
+      <h1 className="text-4xl font-bold text-primary">
+        Pure A1 - Matrícula individual
+      </h1>
+      <div className="flex flex-wrap last:gap-6">
+        {plans.map((plan, index) => (
+          <div key={index}>
+            <Card className="relative flex flex-col">
+              {plan.savings && <div className="ribbon">{plan.savings}</div>}
+              <CardHeader>
+                <CardTitle>{plan.title}</CardTitle>
+                <CardDescription>{plan.description}</CardDescription>
+              </CardHeader>
+              <CardContent className="mb-4 flex-grow">
+                <p className="mb-2 text-2xl font-bold text-primary">
+                  {plan.price}
+                </p>
+                <p className="mb-4 text-sm text-muted-foreground">
+                  Duración: {plan.duration}
+                </p>
+                <ul className="space-y-2">
+                  {plan.features.map((feature, featureIndex) => (
+                    <li key={featureIndex} className="flex items-start">
+                      <Check className="mr-2 mt-0.5 h-5 w-5 text-green-500" />
+                      <span className="text-sm">{feature}</span>
+                    </li>
+                  ))}
+                </ul>
+              </CardContent>
+
+              <Link href={plan.href}>
+                <Button className="mt-1 w-full">Seleccionar Plan</Button>
+              </Link>
+            </Card>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+const BeginnersPlusPureA1 = ({ planId }: { planId: string }) => {
+  const plans = [
+    {
+      href: `/checkout/${planId}/one-time-level`,
+      title: "Pago único por nivel",
+      description: "Matrícula individual",
+      price: "530 mil COP / nivel",
+      duration: "2 meses",
+      features: [
+        "32 clases de una hora (4 por semana)",
+        "Acceso a la plataforma de aprendizaje",
+        "Grabaciones de las clases",
+        "Todos los materiales",
+        "Evaluación al final del nivel",
+        "1 cupo en un grupo de máximo 5 personas",
+      ],
+    },
+
+    {
+      href: `/checkout/${planId}/one-time`,
+      title: "Programa completo",
+      description: "Matrícula individual - Programa completo",
+      price: "1 millón COP / programa",
+      duration: "4 meses (2 niveles)",
+      features: [
+        "64 clases de una hora (4 por semana)",
+        "Acceso a la plataforma de aprendizaje",
+        "Grabaciones de las clases",
+        "Todos los materiales",
+        "Evaluación al final de cada nivel",
+        "1 cupo en un grupo de máximo 5 personas",
+      ],
+      savings: "Ahorras 200 mil pesos",
+    },
+  ];
+
+  return (
+    <div className="container mx-auto flex flex-col items-center justify-center gap-12 border-t py-16">
+      <h1 className="text-4xl font-bold text-primary">
+        Beginners + Pure A1 - Matrícula individual
+      </h1>
+      <div className="flex flex-wrap last:gap-6">
+        {plans.map((plan, index) => (
+          <div key={index}>
+            <Card className="relative flex flex-col">
+              {plan.savings && <div className="ribbon">{plan.savings}</div>}
+              <CardHeader>
+                <CardTitle>{plan.title}</CardTitle>
+                <CardDescription>{plan.description}</CardDescription>
+              </CardHeader>
+              <CardContent className="mb-4 flex-grow">
+                <p className="mb-2 text-2xl font-bold text-primary">
+                  {plan.price}
+                </p>
+                <p className="mb-4 text-sm text-muted-foreground">
+                  Duración: {plan.duration}
+                </p>
+                <ul className="space-y-2">
+                  {plan.features.map((feature, featureIndex) => (
+                    <li key={featureIndex} className="flex items-start">
+                      <Check className="mr-2 mt-0.5 h-5 w-5 text-green-500" />
+                      <span className="text-sm">{feature}</span>
+                    </li>
+                  ))}
+                </ul>
+              </CardContent>
+
+              <Link href={plan.href}>
+                <Button className="mt-1 w-full">Seleccionar Plan</Button>
+              </Link>
+            </Card>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+const FromA2ToB1 = ({ planId }: { planId: string }) => {
+  const plans = [
+    {
+      href: `/checkout/${planId}/one-time-level`,
+      title: "Pago único por nivel",
+      description: "Matrícula individual",
+      price: "530 mil COP / nivel",
+      duration: "2 meses",
+      features: [
+        "32 clases de una hora (4 por semana)",
+        "Acceso a la plataforma de aprendizaje",
+        "Grabaciones de las clases",
+        "Todos los materiales",
+        "Evaluación al final del nivel",
+        "1 cupo en un grupo de máximo 5 personas",
+      ],
+    },
+
+    {
+      href: `/checkout/${planId}/one-time`,
+      title: "Programa completo",
+      description: "Matrícula individual - Programa completo",
+      price: "1 millón COP / programa",
+      duration: "4 meses (2 niveles)",
+      features: [
+        "64 clases de una hora (4 por semana)",
+        "Acceso a la plataforma de aprendizaje",
+        "Grabaciones de las clases",
+        "Todos los materiales",
+        "Evaluación al final de cada nivel",
+        "1 cupo en un grupo de máximo 5 personas",
+      ],
+      savings: "Ahorras 200 mil pesos",
+    },
+  ];
+
+  return (
+    <div className="container mx-auto flex flex-col items-center justify-center gap-12 border-t py-16">
+      <h1 className="text-4xl font-bold text-primary">
+        From A2 to B1 - Matrícula individual
+      </h1>
+      <div className="flex flex-wrap last:gap-6">
+        {plans.map((plan, index) => (
+          <div key={index}>
+            <Card className="relative flex flex-col">
+              {plan.savings && <div className="ribbon">{plan.savings}</div>}
+              <CardHeader>
+                <CardTitle>{plan.title}</CardTitle>
+                <CardDescription>{plan.description}</CardDescription>
+              </CardHeader>
+              <CardContent className="mb-4 flex-grow">
+                <p className="mb-2 text-2xl font-bold text-primary">
+                  {plan.price}
+                </p>
+                <p className="mb-4 text-sm text-muted-foreground">
+                  Duración: {plan.duration}
+                </p>
+                <ul className="space-y-2">
+                  {plan.features.map((feature, featureIndex) => (
+                    <li key={featureIndex} className="flex items-start">
+                      <Check className="mr-2 mt-0.5 h-5 w-5 text-green-500" />
+                      <span className="text-sm">{feature}</span>
+                    </li>
+                  ))}
+                </ul>
+              </CardContent>
+
+              <Link href={plan.href}>
+                <Button className="mt-1 w-full">Seleccionar Plan</Button>
+              </Link>
+            </Card>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+const FromB1ToB2 = ({ planId }: { planId: string }) => {
+  const plans = [
+    {
+      href: `/checkout/${planId}/one-time-level`,
+      title: "Pago único por nivel",
+      description: "Matrícula individual",
+      price: "530 mil COP / nivel",
+      duration: "2 meses",
+      features: [
+        "32 clases de una hora (4 por semana)",
+        "Acceso a la plataforma de aprendizaje",
+        "Grabaciones de las clases",
+        "Todos los materiales",
+        "Evaluación al final del nivel",
+        "1 cupo en un grupo de máximo 5 personas",
+      ],
+    },
+
+    {
+      href: `/checkout/${planId}/one-time`,
+      title: "Programa completo",
+      description: "Matrícula individual - Programa completo",
+      price: "1 millón COP / programa",
+      duration: "4 meses (2 niveles)",
+      features: [
+        "64 clases de una hora (4 por semana)",
+        "Acceso a la plataforma de aprendizaje",
+        "Grabaciones de las clases",
+        "Todos los materiales",
+        "Evaluación al final de cada nivel",
+        "1 cupo en un grupo de máximo 5 personas",
+      ],
+      savings: "Ahorras 200 mil pesos",
+    },
+  ];
+
+  return (
+    <div className="container mx-auto flex flex-col items-center justify-center gap-12 border-t py-16">
+      <h1 className="text-4xl font-bold text-primary">
+        From B1 to B2 - Matrícula individual
+      </h1>
+      <div className="flex flex-wrap last:gap-6">
+        {plans.map((plan, index) => (
+          <div key={index}>
+            <Card className="relative flex flex-col">
+              {plan.savings && <div className="ribbon">{plan.savings}</div>}
+              <CardHeader>
+                <CardTitle>{plan.title}</CardTitle>
+                <CardDescription>{plan.description}</CardDescription>
+              </CardHeader>
+              <CardContent className="mb-4 flex-grow">
+                <p className="mb-2 text-2xl font-bold text-primary">
+                  {plan.price}
+                </p>
+                <p className="mb-4 text-sm text-muted-foreground">
+                  Duración: {plan.duration}
+                </p>
+                <ul className="space-y-2">
+                  {plan.features.map((feature, featureIndex) => (
+                    <li key={featureIndex} className="flex items-start">
+                      <Check className="mr-2 mt-0.5 h-5 w-5 text-green-500" />
+                      <span className="text-sm">{feature}</span>
+                    </li>
+                  ))}
+                </ul>
+              </CardContent>
+
+              <Link href={plan.href}>
+                <Button className="mt-1 w-full">Seleccionar Plan</Button>
+              </Link>
+            </Card>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+const IntensiveB2 = ({ planId }: { planId: string }) => {
+  const plans = [
+    {
+      href: `/checkout/${planId}/one-time-level`,
+      title: "Pago único por nivel",
+      description: "Matrícula individual",
+      price: "530 mil COP / nivel",
+      duration: "2 meses",
+      features: [
+        "32 clases de una hora (4 por semana)",
+        "Acceso a la plataforma de aprendizaje",
+        "Grabaciones de las clases",
+        "Todos los materiales",
+        "Evaluación al final del nivel",
+        "1 cupo en un grupo de máximo 5 personas",
+      ],
+    },
+
+    {
+      href: `/checkout/${planId}/one-time`,
+      title: "Programa completo",
+      description: "Matrícula individual - Programa completo",
+      price: "1 millón COP / programa",
+      duration: "4 meses (2 niveles)",
+      features: [
+        "64 clases de una hora (4 por semana)",
+        "Acceso a la plataforma de aprendizaje",
+        "Grabaciones de las clases",
+        "Todos los materiales",
+        "Evaluación al final de cada nivel",
+        "1 cupo en un grupo de máximo 5 personas",
+      ],
+      savings: "Ahorras 200 mil pesos",
+    },
+  ];
+
+  return (
+    <div className="container mx-auto flex flex-col items-center justify-center gap-12 border-t py-16">
+      <h1 className="text-4xl font-bold text-primary">
+        Intensive B2 - Matrícula individual
+      </h1>
+      <div className="flex flex-wrap last:gap-6">
+        {plans.map((plan, index) => (
+          <div key={index}>
+            <Card className="relative flex flex-col">
+              {plan.savings && <div className="ribbon">{plan.savings}</div>}
+              <CardHeader>
+                <CardTitle>{plan.title}</CardTitle>
+                <CardDescription>{plan.description}</CardDescription>
+              </CardHeader>
+              <CardContent className="mb-4 flex-grow">
+                <p className="mb-2 text-2xl font-bold text-primary">
+                  {plan.price}
+                </p>
+                <p className="mb-4 text-sm text-muted-foreground">
+                  Duración: {plan.duration}
+                </p>
+                <ul className="space-y-2">
+                  {plan.features.map((feature, featureIndex) => (
+                    <li key={featureIndex} className="flex items-start">
+                      <Check className="mr-2 mt-0.5 h-5 w-5 text-green-500" />
+                      <span className="text-sm">{feature}</span>
+                    </li>
+                  ))}
+                </ul>
+              </CardContent>
+
+              <Link href={plan.href}>
+                <Button className="mt-1 w-full">Seleccionar Plan</Button>
+              </Link>
+            </Card>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+const BusinessEnglish = ({ planId }: { planId: string }) => {
+  const plans = [
+    {
+      href: `/checkout/${planId}/one-time-level`,
+      title: "Pago único por nivel",
+      description: "Matrícula individual",
+      price: "530 mil COP / nivel",
+      duration: "2 meses",
+      features: [
+        "32 clases de una hora (4 por semana)",
+        "Acceso a la plataforma de aprendizaje",
+        "Grabaciones de las clases",
+        "Todos los materiales",
+        "Evaluación al final del nivel",
+        "1 cupo en un grupo de máximo 5 personas",
+      ],
+    },
+
+    {
+      href: `/checkout/${planId}/one-time`,
+      title: "Programa completo",
+      description: "Matrícula individual - Programa completo",
+      price: "1 millón COP / programa",
+      duration: "4 meses (2 niveles)",
+      features: [
+        "64 clases de una hora (4 por semana)",
+        "Acceso a la plataforma de aprendizaje",
+        "Grabaciones de las clases",
+        "Todos los materiales",
+        "Evaluación al final de cada nivel",
+        "1 cupo en un grupo de máximo 5 personas",
+      ],
+      savings: "Ahorras 200 mil pesos",
+    },
+  ];
+
+  return (
+    <div className="container mx-auto flex flex-col items-center justify-center gap-12 border-t py-16">
+      <h1 className="text-4xl font-bold text-primary">
+        Business English - Matrícula individual
       </h1>
       <div className="flex flex-wrap last:gap-6">
         {plans.map((plan, index) => (
