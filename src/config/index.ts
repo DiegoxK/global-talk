d59aa41f1735025c88d5ddf7f36e8d2b2ec8eff5
DiegoxK <diego.stv@outlook.es>
2024-10-29 14:09:54 -0500

    Refactor pricing structure to include detailed price levels and features for each program in the site configuration

--- src/config/index.ts
+++ src/config/index.ts
@@ -18,9 +18,14 @@ type PricingKey =
   | "from_b1_to_b2";
 
 interface PricingDetails {
-  price: string;
+  price: {
+    level: string;
+    complete: string;
+  };
   name: string;
   description: string;
+  features: string[];
+  extra?: string;
 }
 
 export type Pricing = Record<PricingKey, PricingDetails>;
@@ -66,39 +71,118 @@ export const siteConfig: SiteConfig = {
 
   pricing: {
     beginners_a0: {
-      price: "10000",
+      price: {
+        level: "10000",
+        complete: "10000",
+      },
       name: "Beginners A0",
       description: "Programa Beginners A0, 2 niveles (4 meses)",
+      features: [
+        "16 clases de una hora (4 por semana)",
+        "Acceso a la plataforma de aprendizaje",
+        "Grabaciones de las clases",
+        "Todos los materiales",
+        "Evaluación al final de cada nivel",
+        "1 cupo en un grupo de máximo 5 personas",
+      ],
+      extra:
+        "Este plan es recurrente y se cobrará 300.000 pesos mensualmente durante el transcurso del programa (4 meses).",
     },
     pure_a1: {
-      price: "10000",
+      price: {
+        level: "10000",
+        complete: "10000",
+      },
       name: "Pure A1",
       description: "Programa Pure A1, 2 niveles (4 meses)",
+      features: [
+        "16 clases de una hora (4 por semana)",
+        "Acceso a la plataforma de aprendizaje",
+        "Grabaciones de las clases",
+        "Todos los materiales",
+        "Evaluación al final de cada nivel",
+        "1 cupo en un grupo de máximo 5 personas",
+      ],
     },
     beginners_plus_pure_a1: {
-      price: "10000",
+      price: {
+        level: "10000",
+        complete: "10000",
+      },
       name: "Beginners + Pure A1",
       description: "Programa Beginners + Pure A1, 2 niveles (4 meses)",
+      features: [
+        "16 clases de una hora (4 por semana)",
+        "Acceso a la plataforma de aprendizaje",
+        "Grabaciones de las clases",
+        "Todos los materiales",
+        "Evaluación al final de cada nivel",
+        "1 cupo en un grupo de máximo 5 personas",
+      ],
     },
     from_a2_to_b1: {
-      price: "10000",
+      price: {
+        level: "10000",
+        complete: "10000",
+      },
       name: "From A2 to B1",
       description: "Programa From A2 to B1, 2 niveles (4 meses)",
+      features: [
+        "16 clases de una hora (4 por semana)",
+        "Acceso a la plataforma de aprendizaje",
+        "Grabaciones de las clases",
+        "Todos los materiales",
+        "Evaluación al final de cada nivel",
+        "1 cupo en un grupo de máximo 5 personas",
+      ],
     },
     from_b1_to_b2: {
-      price: "10000",
+      price: {
+        level: "10000",
+        complete: "10000",
+      },
       name: "From B1 to B2",
       description: "Programa From B1 to B2, 2 niveles (4 meses)",
+      features: [
+        "16 clases de una hora (4 por semana)",
+        "Acceso a la plataforma de aprendizaje",
+        "Grabaciones de las clases",
+        "Todos los materiales",
+        "Evaluación al final de cada nivel",
+        "1 cupo en un grupo de máximo 5 personas",
+      ],
     },
     intensive_b2: {
-      price: "10000",
+      price: {
+        level: "10000",
+        complete: "10000",
+      },
       name: "Intensive B2",
       description: "Programa Intensive B2, 2 niveles (4 meses)",
+      features: [
+        "16 clases de una hora (4 por semana)",
+        "Acceso a la plataforma de aprendizaje",
+        "Grabaciones de las clases",
+        "Todos los materiales",
+        "Evaluación al final de cada nivel",
+        "1 cupo en un grupo de máximo 5 personas",
+      ],
     },
     business_english: {
-      price: "10000",
+      price: {
+        level: "10000",
+        complete: "10000",
+      },
       name: "Business English",
       description: "Programa Business English, 2 niveles (4 meses)",
+      features: [
+        "16 clases de una hora (4 por semana)",
+        "Acceso a la plataforma de aprendizaje",
+        "Grabaciones de las clases",
+        "Todos los materiales",
+        "Evaluación al final de cada nivel",
+        "1 cupo en un grupo de máximo 5 personas",
+      ],
     },
   },
 };
