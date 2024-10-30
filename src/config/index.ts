218bb77a56497c7c67b64848ad0c7fe6a8e02857
DiegoxK <diego.stv@outlook.es>
2024-10-30 13:36:53 -0500

    Refactor siteConfig pricing descriptions and features for clarity and consistency across multiple language programs

--- src/config/index.ts
+++ src/config/index.ts
@@ -78,6 +78,7 @@ export const siteConfig: SiteConfig = {
     },
   ],
 
+  // THESE ARE THE REAL PRICING VALUES USED IN EPAYCO
   pricing: {
     beginners_a0: {
       price: {
@@ -86,31 +87,33 @@ export const siteConfig: SiteConfig = {
       },
       name: "Beginners A0",
       description: {
-        level: "Programa Beginners A0, 2 niveles (4 meses)",
-        complete: "Programa Beginners A0, 2 niveles (4 meses)",
+        level: "Programa Beginners A0, ( 2 meses, 1 nivel )",
+        complete: "Programa Beginners A0, ( 4 meses. 2 niveles )",
       },
       features: {
         level: [
-          "16 clases de una hora (4 por semana)",
+          "32 clases de una hora (4 por semana)",
           "Acceso a la plataforma de aprendizaje",
           "Grabaciones de las clases",
-          "Todos los materiales",
-          "Evaluación al final de cada nivel",
-          "1 cupo en un grupo de máximo 5 personas",
+          "Material de estudio",
+          "Evaluación al final del nivel",
+          "1 cupo en clases de máximo 5 personas",
         ],
         complete: [
-          "16 clases de una hora (4 por semana)",
+          "64 clases de una hora (4 por semana)",
+          "Programa completo: 2 niveles",
           "Acceso a la plataforma de aprendizaje",
           "Grabaciones de las clases",
-          "Todos los materiales",
+          "Material de estudio",
           "Evaluación al final de cada nivel",
-          "1 cupo en un grupo de máximo 5 personas",
+          "1 cupo en clases de máximo 5 personas",
         ],
       },
       extra: {
         level:
-          "Este plan es recurrente y se cobrará 300.000 pesos mensualmente durante el transcurso del programa (4 meses).",
-        complete: "asdsad",
+          "Este plan consiste en el pago de un unico nivel con duracion de 2 meses. Al finalizar su nivel, sera notificado en la aplicación acerca de la posibilidad de comprar el siguiente nivel.",
+        complete:
+          "Este plan consiste en el pago unico del programa completo con duracion de 4 meses en el transcurso de 2 niveles.",
       },
     },
     pure_a1: {
@@ -120,26 +123,33 @@ export const siteConfig: SiteConfig = {
       },
       name: "Pure A1",
       description: {
-        level: "Programa Pure A1, 2 niveles (4 meses)",
-        complete: "Programa Pure A1, 2 niveles (4 meses)",
+        level: "Programa Pure A1, ( 2 meses, 1 nivel )",
+        complete: "Programa Pure A1, ( 4 meses. 2 niveles )",
       },
       features: {
         level: [
-          "16 clases de una hora (4 por semana)",
+          "32 clases de una hora (4 por semana)",
           "Acceso a la plataforma de aprendizaje",
           "Grabaciones de las clases",
-          "Todos los materiales",
+          "Material de estudio",
           "Evaluación al final de cada nivel",
-          "1 cupo en un grupo de máximo 5 personas",
+          "1 cupo en clases de máximo 5 personas",
         ],
         complete: [
-          "16 clases de una hora (4 por semana)",
+          "64 clases de una hora (4 por semana)",
+          "Programa completo: 2 niveles",
           "Acceso a la plataforma de aprendizaje",
           "Grabaciones de las clases",
           "Todos los materiales",
           "Evaluación al final de cada nivel",
           "1 cupo en un grupo de máximo 5 personas",
         ],
+      },
+      extra: {
+        level:
+          "Este plan consiste en el pago de un unico nivel con duracion de 2 meses. Al finalizar su nivel, sera notificado en la aplicación acerca de la posibilidad de comprar el siguiente nivel.",
+        complete:
+          "Este plan consiste en el pago unico del programa completo con duracion de 4 meses en el transcurso de 2 niveles.",
       },
     },
     beginners_plus_pure_a1: {
@@ -149,26 +159,25 @@ export const siteConfig: SiteConfig = {
       },
       name: "Beginners + Pure A1",
       description: {
-        level: "Programa Beginners + Pure A1, 2 niveles (4 meses)",
-        complete: "Programa Beginners + Pure A1, 2 niveles (4 meses)",
+        level: "",
+        complete: "Programa Beginners + Pure A1, ( 8 meses. 4 niveles )",
       },
       features: {
-        level: [
-          "16 clases de una hora (4 por semana)",
-          "Acceso a la plataforma de aprendizaje",
-          "Grabaciones de las clases",
-          "Todos los materiales",
-          "Evaluación al final de cada nivel",
-          "1 cupo en un grupo de máximo 5 personas",
-        ],
+        level: [],
         complete: [
-          "16 clases de una hora (4 por semana)",
+          "124 clases de una hora (4 por semana)",
           "Acceso a la plataforma de aprendizaje",
+          "Programa completo: 4 niveles",
           "Grabaciones de las clases",
           "Todos los materiales",
           "Evaluación al final de cada nivel",
           "1 cupo en un grupo de máximo 5 personas",
         ],
+      },
+      extra: {
+        level: "",
+        complete:
+          "Este plan consiste en el pago unico del programa completo con duracion de 8 meses en el transcurso de 4 niveles.",
       },
     },
     from_a2_to_b1: {
@@ -178,26 +187,35 @@ export const siteConfig: SiteConfig = {
       },
       name: "From A2 to B1",
       description: {
-        level: "Programa From A2 to B1, 2 niveles (4 meses)",
-        complete: "Programa From A2 to B1, 2 niveles (4 meses)",
+        level: "Programa From A2 to B1, ( 2 meses, 1 nivel )",
+        complete: "Programa From A2 to B1, ( 10 meses. 5 niveles )",
       },
       features: {
         level: [
-          "16 clases de una hora (4 por semana)",
+          "32 clases de una hora (4 por semana)",
+          "Prueba de nivel",
           "Acceso a la plataforma de aprendizaje",
           "Grabaciones de las clases",
-          "Todos los materiales",
+          "Material de estudio",
           "Evaluación al final de cada nivel",
-          "1 cupo en un grupo de máximo 5 personas",
+          "1 cupo en clases de máximo 5 personas",
         ],
         complete: [
-          "16 clases de una hora (4 por semana)",
+          "160 clases de una hora (4 por semana)",
+          "Prueba de nivel",
           "Acceso a la plataforma de aprendizaje",
+          "Programa completo: 5 niveles",
           "Grabaciones de las clases",
           "Todos los materiales",
           "Evaluación al final de cada nivel",
           "1 cupo en un grupo de máximo 5 personas",
         ],
+      },
+      extra: {
+        level:
+          "Este plan consiste en el pago de un unico nivel con duracion de 2 meses. Al finalizar su nivel, sera notificado en la aplicación acerca de la posibilidad de comprar el siguiente nivel.",
+        complete:
+          "Este plan consiste en el pago unico del programa completo con duracion de 10 meses en el transcurso de 5 niveles.",
       },
     },
     from_b1_to_b2: {
@@ -207,26 +225,33 @@ export const siteConfig: SiteConfig = {
       },
       name: "From B1 to B2",
       description: {
-        level: "Programa From B1 to B2, 2 niveles (4 meses)",
-        complete: "Programa From B1 to B2, 2 niveles (4 meses)",
+        level: "Programa From B1 to B2, ( 2 meses, 1 nivel )",
+        complete: "Programa From B1 to B2, ( 10 meses. 5 niveles )",
       },
       features: {
         level: [
-          "16 clases de una hora (4 por semana)",
+          "32 clases de una hora (4 por semana)",
           "Acceso a la plataforma de aprendizaje",
           "Grabaciones de las clases",
-          "Todos los materiales",
+          "Material de estudio",
           "Evaluación al final de cada nivel",
-          "1 cupo en un grupo de máximo 5 personas",
+          "1 cupo en clases de máximo 5 personas",
         ],
         complete: [
-          "16 clases de una hora (4 por semana)",
+          "160 clases de una hora (4 por semana)",
           "Acceso a la plataforma de aprendizaje",
+          "Programa completo: 5 niveles",
           "Grabaciones de las clases",
           "Todos los materiales",
           "Evaluación al final de cada nivel",
           "1 cupo en un grupo de máximo 5 personas",
         ],
+      },
+      extra: {
+        level:
+          "Este plan consiste en el pago de un unico nivel con duracion de 2 meses. Al finalizar su nivel, sera notificado en la aplicación acerca de la posibilidad de comprar el siguiente nivel.",
+        complete:
+          "Este plan consiste en el pago unico del programa completo con duracion de 10 meses en el transcurso de 5 niveles.",
       },
     },
     intensive_b2: {
@@ -236,26 +261,26 @@ export const siteConfig: SiteConfig = {
       },
       name: "Intensive B2",
       description: {
-        level: "Programa Intensive B2, 2 niveles (4 meses)",
-        complete: "Programa Intensive B2, 2 niveles (4 meses)",
+        level: "",
+        complete: "Programa Intensive B2, ( 10 meses. 5 niveles )",
       },
       features: {
-        level: [
-          "16 clases de una hora (4 por semana)",
-          "Acceso a la plataforma de aprendizaje",
-          "Grabaciones de las clases",
-          "Todos los materiales",
-          "Evaluación al final de cada nivel",
-          "1 cupo en un grupo de máximo 5 personas",
-        ],
+        level: [],
         complete: [
-          "16 clases de una hora (4 por semana)",
+          "120 clases de una hora (4 por semana)",
           "Acceso a la plataforma de aprendizaje",
+          "Prueba de nivel",
+          "Programa completo: 2 niveles",
           "Grabaciones de las clases",
           "Todos los materiales",
           "Evaluación al final de cada nivel",
           "1 cupo en un grupo de máximo 5 personas",
         ],
+      },
+      extra: {
+        level: "",
+        complete:
+          "Este plan consiste en el pago unico del programa completo con duracion de 3 meses en el transcurso de 2 niveles.",
       },
     },
     business_english: {
@@ -265,26 +290,33 @@ export const siteConfig: SiteConfig = {
       },
       name: "Business English",
       description: {
-        level: "Programa Business English, 2 niveles (4 meses)",
-        complete: "Programa Business English, 2 niveles (4 meses)",
+        level: "Programa Business English, ( 2 meses, 1 nivel )",
+        complete: "Programa Business English, ( 10 meses. 5 niveles )",
       },
       features: {
         level: [
-          "16 clases de una hora (4 por semana)",
+          "32 clases de una hora (4 por semana)",
           "Acceso a la plataforma de aprendizaje",
           "Grabaciones de las clases",
-          "Todos los materiales",
+          "Material de estudio",
           "Evaluación al final de cada nivel",
-          "1 cupo en un grupo de máximo 5 personas",
+          "1 cupo en clases de máximo 5 personas",
         ],
         complete: [
-          "16 clases de una hora (4 por semana)",
+          "160 clases de una hora (4 por semana)",
           "Acceso a la plataforma de aprendizaje",
+          "Programa completo: 5 niveles",
           "Grabaciones de las clases",
           "Todos los materiales",
           "Evaluación al final de cada nivel",
           "1 cupo en un grupo de máximo 5 personas",
         ],
+      },
+      extra: {
+        level:
+          "Este plan consiste en el pago de un unico nivel con duracion de 2 meses. Al finalizar su nivel, sera notificado en la aplicación acerca de la posibilidad de comprar el siguiente nivel.",
+        complete:
+          "Este plan consiste en el pago unico del programa completo con duracion de 10 meses en el transcurso de 5 niveles.",
       },
     },
   },
