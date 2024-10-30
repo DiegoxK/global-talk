35607e5ce5f8f6e1fb5872ab24c5cf0764aae9fb
DiegoxK <diego.stv@outlook.es>
2024-10-30 15:29:31 -0500

    Integrate MobileSidebar component: improve layout responsiveness and user display for the academy route's main interface

--- src/app/(routes)/academy/layout.tsx
+++ src/app/(routes)/academy/layout.tsx
@@ -3,6 +3,7 @@ import { redirect } from "next/navigation";
 import Sidebar from "./_components/ui/sidebar";
 import { Logo } from "@/vectors/logo";
 import { api } from "@/trpc/server";
+import MobileSidebar from "./_components/ui/mobile-sidebar";
 
 export default async function RootLayout({
   children,
@@ -23,10 +24,22 @@ export default async function RootLayout({
   return (
     <div className="flex">
       <Sidebar user={session.user} program={program} firstLevel={firstLevel} />
-      <div className="mx-4 mt-4 flex w-full flex-col">
-        <div className="flex w-full items-center justify-center gap-1 rounded-lg bg-primary py-10 text-3xl font-extrabold text-white">
-          <Logo className="fill-white" height={40} width={38} />
-          lobal talk medallo
+
+      <div className="flex w-full flex-col md:mx-4 md:mt-4">
+        <div
+          style={{ backgroundSize: "100px" }}
+          className="bg-pattern flex w-full items-center justify-between gap-2 px-7 py-10 text-3xl font-extrabold text-white md:justify-center md:gap-1 md:rounded-lg md:px-0"
+        >
+          <MobileSidebar
+            user={session.user}
+            program={program}
+            firstLevel={firstLevel}
+          />
+          <Logo className="hidden fill-white md:block" height={40} width={38} />
+          <div className="flex">
+            <p className="md:hidden">G</p>
+            <p>lobal talk medallo</p>
+          </div>
         </div>
         <main className="relative grow px-0">{children}</main>
       </div>
