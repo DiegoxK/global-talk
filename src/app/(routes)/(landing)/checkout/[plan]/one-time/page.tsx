97cd5b5b4045bf78314b0425d476c624d9e5bbaa
juliofuego <juliancuenta0@gmail.com>
2024-11-19 11:25:20 -0500

    pasarela on

--- src/app/(routes)/(landing)/checkout/[plan]/one-time/page.tsx
+++ src/app/(routes)/(landing)/checkout/[plan]/one-time/page.tsx
@@ -231,7 +231,7 @@ export default function Recurrent({ params }: { params: { plan: string } }) {
         transactionType: "COMPLETE",
         confirmation: `https://www.academiaglobtm.com/api/checkout/confirmation`,
         response: "https://www.academiaglobtm.com/response",
-        test: "true",
+        test: "false",
       };
 
       createSession(paymentDetails);
